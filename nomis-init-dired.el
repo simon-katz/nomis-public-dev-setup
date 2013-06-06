e3493fa373ffbd2eb849f8c153446991bc4369e8
Simon Katz <git@nomistech.com>
2013-06-06 13:11:09 +0100

    Improve your "Explorer"-like behaviour in dired.

--- nomis-init-dired.el
+++ nomis-init-dired.el
@@ -10,58 +10,59 @@
 ;;;; ___________________________________________________________________________
 ;;;; ---- "Explorer"-like behaviour ----
 ;;;; Navigate in one window; display contents in other window.
-;;;; I suggest having "A" in your dired-listing-switches so that . and ..
-;;;; are not displayed.
 ;;;; Does not show the contents of the current directory (.) because the
 ;;;; underlying functionality (dired-display-file) doesn't.
+;;;; I suggest having "A" in your dired-listing-switches so that . and ..
+;;;; are not displayed.
 
 (defvar nomis-dired-do-display-file t)
 
-(defun nomis-toggle-dired-do-display-file ()
-  "Nomis Explorer-like dired:
-Toggle whether file/directories are displayed (in other window) when navigating."
+(defun nomis-dired-find-file-if-dir ()
+ "Nomis Explorer-like dired:
+If selected entry is a directory go into it."
   (interactive)
-  (setq nomis-dired-do-display-file (not nomis-dired-do-display-file)))
-
-(defun nomis-dired-maybe-display-file ()
-  (when nomis-dired-do-display-file
-    (dired-display-file)))
+  (when (file-directory-p (dired-get-filename nil t))
+    (dired-find-file)))
 
 (defun nomis-dired-previous-line (arg)
   "Nomis Explorer-like dired:
 Move up lines and maybe display file in other window."
   (interactive "p")
   (dired-previous-line arg)
-  (nomis-dired-maybe-display-file))
+  (dired-display-file))
 
 (defun nomis-dired-next-line (arg)
   "Nomis Explorer-like dired:
 Move down lines and maybe display file in other window."
   (interactive "p")
   (dired-next-line arg)
-  (nomis-dired-maybe-display-file))
+  (dired-display-file))
 
 (defun nomis-dired-into ()
- "Nomis Explorer-like dired:
+  "Nomis Explorer-like dired:
 Go into selected directory and maybe display its contents in other window."
   (interactive)
-  (when (file-directory-p (dired-get-filename nil t))
-    (dired-find-file))
-  (nomis-dired-maybe-display-file))
+  (nomis-dired-find-file-if-dir)
+  (dired-display-file))
 
-(defun nomis-dired-back ()
+(defun nomis-dired-up-directory ()
   "Nomis Explorer-like dired:
 Go up a directory and maybe display its contents in other window."
   (interactive)
   (dired-up-directory)
-  (nomis-dired-maybe-display-file))
+  (dired-display-file))
 
 (add-hook
  'dired-mode-hook
  (lambda ()
    (define-key dired-mode-map (kbd "M-<RET>") 'dired-display-file)
    
-   (define-key dired-mode-map (kbd "M-<up>") 'nomis-dired-previous-line)
-   (define-key dired-mode-map (kbd "M-<down>") 'nomis-dired-next-line)
-   (define-key dired-mode-map (kbd "M-<left>") 'nomis-dired-back)
-   (define-key dired-mode-map (kbd "M-<right>") 'nomis-dired-into)))
+   (define-key dired-mode-map (kbd "M-<up>") 'dired-previous-line)
+   (define-key dired-mode-map (kbd "M-<down>") 'dired-next-line)
+   (define-key dired-mode-map (kbd "M-<left>") 'dired-up-directory)
+   (define-key dired-mode-map (kbd "M-<right>") 'nomis-dired-find-file-if-dir)
+   
+   (define-key dired-mode-map (kbd "M-S-<up>") 'nomis-dired-previous-line)
+   (define-key dired-mode-map (kbd "M-S-<down>") 'nomis-dired-next-line)
+   (define-key dired-mode-map (kbd "M-S-<left>") 'nomis-dired-up-directory)
+   (define-key dired-mode-map (kbd "M-S-<right>") 'nomis-dired-into)))
