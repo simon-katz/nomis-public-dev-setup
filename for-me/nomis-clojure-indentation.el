21154072cf50ec3e9a81fc33cacc790fc843487f
Simon Katz <git@nomistech.com>
2013-06-21 17:40:58 +0100

    Add Emacs intentation for Midje facts.

--- for-me/nomis-clojure-indentation.el
+++ for-me/nomis-clojure-indentation.el
@@ -5,6 +5,7 @@
 (require 'clojure-mode)
 
 (define-clojure-indent
+  ;; Ring and Compojure
   (defroutes 'defun)
   (GET 2)
   (POST 2)
@@ -12,7 +13,9 @@
   (DELETE 2)
   (HEAD 2)
   (ANY 2)
-  (context 2))
+  (context 2)
+  ;; Midje
+  (fact 1))
 
 ;;;; ___________________________________________________________________________
 
