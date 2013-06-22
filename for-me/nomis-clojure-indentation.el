a3ed6911e49bcc6dba5daf541195a9d12b533c0e
Simon Katz <git@nomistech.com>
2013-06-22 09:36:13 +0100

    Improve clojure-mode indentation.
    Don't load clojure-mode until it's asked for.
    Add more for Midje.

--- for-me/nomis-clojure-indentation.el
+++ for-me/nomis-clojure-indentation.el
@@ -2,20 +2,22 @@
 
 ;;;; From https://github.com/weavejester/compojure/wiki/Emacs-indentation.
 
-(require 'clojure-mode)
-
-(define-clojure-indent
-  ;; Ring and Compojure
-  (defroutes 'defun)
-  (GET 2)
-  (POST 2)
-  (PUT 2)
-  (DELETE 2)
-  (HEAD 2)
-  (ANY 2)
-  (context 2)
-  ;; Midje
-  (fact 1))
+(eval-after-load 'clojure-mode
+  '(define-clojure-indent
+     ;; Ring and Compojure
+     (defroutes 'defun)
+     (GET 2)
+     (POST 2)
+     (PUT 2)
+     (DELETE 2)
+     (HEAD 2)
+     (ANY 2)
+     (context 2)
+     ;; Midje
+     (fact 'defun)
+     (facts 'defun)
+     (against-background 'defun)
+     (provided 0)))
 
 ;;;; ___________________________________________________________________________
 
