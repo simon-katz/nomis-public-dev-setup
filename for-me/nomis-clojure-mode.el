0d7c1945e0e005356a29a09878d8f6013909fa67
Simon Katz <sk@nomistech.com>
2013-08-15 20:51:59 +0100

    Add/move TODO notes.

--- for-me/nomis-clojure-mode.el
+++ for-me/nomis-clojure-mode.el
@@ -18,7 +18,7 @@
 (add-hook 'clojure-mode-hook 'subword-mode)
 (add-hook 'clojure-mode-hook 'generic-lispy-stuff-for-non-repls)
 
-(define-key clojure-mode-map (kbd "RET") 'newline-and-indent) ; TODO: Modularise with same change to emacs-lisp-mode-map (General question: Do you want a generic-lispy-mode-map? Think about maps and modes -- does every mode have a map? If so you want a generic-lispy-mode (as the starter kit has, right?) (Or maybe KIES -- keep the modes separate and duplciate things)
+(define-key clojure-mode-map (kbd "RET") 'newline-and-indent) ; TODO: Modularise with same change to emacs-lisp-mode-map.
 
 ;;;; ___________________________________________________________________________
 
