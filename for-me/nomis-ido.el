13b5daaecdc9842b4f22968e6440bddd15f4f5e1
Simon Katz <sk@nomistech.com>
2013-08-15 21:23:16 +0100

    Add some comments explaining why I want ido-ubiquitous and smex.

--- for-me/nomis-ido.el
+++ for-me/nomis-ido.el
@@ -5,7 +5,7 @@
 ;; TODO: Look at this ido stuff copied from the Starter Kit.
 
 (ido-mode t)
-(ido-ubiquitous t)
+(ido-ubiquitous t) ; e.g. for `describe-function' (C-h f)
 
 (setq ido-default-file-method 'selected-window)
 (setq ido-default-buffer-method 'selected-window)
--- for-me/nomis-smex.el
+++ for-me/nomis-smex.el
@@ -2,6 +2,11 @@
 
 ;;;; ___________________________________________________________________________
 
+;;;; Point of info:
+;;;; One reason why smex is better than execute-extended-command is that
+;;;; smex remembers the last command, whereas execute-extended-command
+;;;; (even with ido-ubiquitous) does not.
+
 (setq smex-save-file (concat user-emacs-directory ".smex-items"))
 (smex-initialize) ; Can be omitted. This might cause a (minimal) delay
                   ; when Smex is auto-initialized on its first run.
