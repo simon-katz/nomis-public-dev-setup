fa75979e5e4a72dadd7bb31b10a4ea8643cf2cf5
Simon Katz <sk@nomistech.com>
2013-08-16 09:59:10 +0100

    Added new file "nomis-environment-os-x" and moved some stuff to there.

--- dot-emacs.el
+++ dot-emacs.el
@@ -50,27 +50,6 @@
       (package-install p))))
 
 ;;;; ___________________________________________________________________________
-
-;;; TODO: Move to new file: "nomis-environment-os-x".
-
-;;;; From https://github.com/technomancy/swank-clojure:
-
-;;; On Mac OS X, Emacs sessions launched from the GUI don't always
-;;; respect your configured $PATH. If Emacs can't find lein, you may
-;;; need to give it some help. The quickest way is probably to add
-;;; this elisp to your config:
-
-;;; FIXME: I can't make this work -- the path has not had my .bashrc
-;; stuff added.
-;; (setenv "PATH" (shell-command-to-string "echo $PATH"))
-;; (shell-command-to-string "echo $PS1")
-;; (progn shell-file-name)
-;;; So hack like this instead for now:
-(setenv "PATH" (concat "~/bin:"
-                       "/usr/local/bin:"
-                       (shell-command-to-string "echo $PATH")))
-
-;;;; ___________________________________________________________________________
 ;;;; ---- load-path ----
 
 (defun nomis-load-file-name ()
@@ -81,6 +60,8 @@
 
 ;;;; ___________________________________________________________________________
 ;;;; ---- Load various files ----
+
+(require 'nomis-environment-os-x)
 
 (require 'nomis-very-general-stuff)
 
