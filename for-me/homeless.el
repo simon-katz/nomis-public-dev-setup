9907e7b91f490b76b5b60dee2312fa2b16484724
Simon Katz <sk@nomistech.com>
2013-08-17 17:05:23 +0100

    Improvements to `revert-all-buffers`.

--- for-me/homeless.el
+++ for-me/homeless.el
@@ -1,16 +1,27 @@
 ;; Init stuff -- homeless -- to maybe move to a better place
 
 ;;;; ___________________________________________________________________________
+;;;; Reverting buffers.
+;;;; Copy-and-hack of http://www.emacswiki.org/emacs/RevertBuffer.
 
-(defun revert-all-buffers ()
-  "Refreshes all open buffers from their respective files."
-  ;; Copied from http://www.emacswiki.org/emacs/RevertBuffer.
-  (interactive)
+(defun _revert-all-buffers (buffer-predicate)
   (dolist (buf (buffer-list))
     (with-current-buffer buf
-      (when (and (buffer-file-name) (not (buffer-modified-p)))
+      (when (and (buffer-file-name) (funcall buffer-predicate buf))
         (revert-buffer t t t) )))
-  (message "Refreshed open files.") )
+  (message "Refreshed open files."))
+
+(defun revert-all-unmodified-buffers ()
+  "Refreshes all open unmodified buffers from their files."
+  (interactive)
+  (_revert-all-buffers (lambda (b) (not (buffer-modified-p b)))) )
+
+(defun revert-all-modified-buffers ()
+  "Refreshes all open modified buffers from their files."
+  ;; Copied from http://www.emacswiki.org/emacs/RevertBuffer, and renamed.
+  (interactive)
+  (when (y-or-n-p "Really revert modified buffers? You will lose stuff.")
+    (_revert-all-buffers 'buffer-modified-p)) )
 
 ;;;; ___________________________________________________________________________
 
