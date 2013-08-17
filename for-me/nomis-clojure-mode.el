a0fa455c526ad302595e123776dfa620dd6625a3
Simon Katz <sk@nomistech.com>
2013-08-17 19:40:29 +0100

    Delete no-longer-necessary hack that removes Starter Kit hack that displays Clojure `fn` as "pretty" italic-y "f".

--- for-me/nomis-clojure-mode.el
+++ for-me/nomis-clojure-mode.el
@@ -3,21 +3,6 @@
 (require 'clojure-mode)
 (require 'nomis-define-lispy-modes)
 
-;; (eval-after-load 'clojure-mode
-;;   ;; Get rid of displaying (fn ...) as italic-y f (f ...),
-;;   ;; which is set up in ".../.emacs.d/starter-kit-lisp.el" using a
-;;   ;; form like this but with font-lock-add-keywords instead of
-;;   ;; font-lock-remove-keywords
-;;   '(font-lock-remove-keywords
-;;     'clojure-mode `(("(\\(fn\\>\\)"
-;;                      (0 (progn (compose-region (match-beginning 1)
-;;                                                (match-end 1) "ƒ")
-;;                                nil))))))
-
-(progn
-  ;; Get rid of displaying (fn ...) as italic-y f (f ...)
-  (remove-hook 'clojure-mode-hook 'esk-pretty-fn))
-
 (add-hook 'clojure-mode-hook 'nomis-lispy-non-repl-setup)
 (add-hook 'clojure-mode-hook 'nomis-clojure-setup)
 
