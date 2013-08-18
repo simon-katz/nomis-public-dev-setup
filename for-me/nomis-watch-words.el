9d1c12bd9c631a390a2fa4bb6887dcf094c2e47e
Simon Katz <sk@nomistech.com>
2013-08-18 16:03:19 +0100

    Fix highlighting of watch words. (Re-instate some lost Emacs Starter Kit functionality and add some extra stuff.)

--- for-me/nomis-watch-words.el
+++ for-me/nomis-watch-words.el
@@ -1,31 +1,64 @@
-;;;; Init stuff -- Watch-words.
+;;;; Init stuff -- Watch words.
+
+(defvar nomis-watch-words
+  '("FIXME"
+    "FIX"
+    "TODO-THINK"
+    "TODO"
+    "REMAINING-ISSUE"
+    "HACK"
+    "REFACTOR"
+    "NOCOMMIT"))
+
+(defun nomis-make-regex-from-watchwords-helper (words beginning-of-word-p)
+  (with-output-to-string
+    (when beginning-of-word-p (princ "\\<"))
+    (princ "\\(")
+    (princ (first words))
+    (dolist (w (rest words))
+      (princ "\\|")
+      (princ w))
+    (princ "\\)")))
+
+(defun nomis-make-regex-from-watchwords/simple ()
+  (nomis-make-regex-from-watchwords-helper nomis-watch-words
+                                           t))
+
+(defun nomis-make-regex-from-watchwords/bracketed ()
+  (let* ((bracketed-watch-words
+          (mapcar (lambda (word) (concat "\\[" word "]"))
+                  nomis-watch-words)))
+    (nomis-make-regex-from-watchwords-helper bracketed-watch-words
+                                             nil)))
 
 (defun add-nomis-watch-words ()
   (font-lock-add-keywords
    nil
-   '(("\\[REMAINING-ISSUE\\]"
+   `((,(nomis-make-regex-from-watchwords/simple)
       0 font-lock-warning-face t)
-     ("\\<\\(REMAINING-ISSUE\\):"
-      1 font-lock-warning-face t)
-     ("\\<\\(TOD\O-THINK\\):"
-      1 font-lock-warning-face t))))
-
-;; I'm getting:
-;;   emacs file mode specification error void-function add-watchwords
-;; (add-hook 'text-mode-hook 'add-watchwords) ; does (font-lock-add-keywords nil '(("\\<\\(FIX\\|TOD\O\\|FIXME\\|HACK\\|REFACTOR\\):" 1 font-lock-warning-face t)))
+     (,(nomis-make-regex-from-watchwords/bracketed)
+      0 font-lock-warning-face t))))
 
 (add-hook 'text-mode-hook 'add-nomis-watch-words)
-(add-hook 'coding-hook    'add-nomis-watch-words)
+(add-hook 'prog-mode-hook 'add-nomis-watch-words)
 
-;;;; Temp stuff for testing the above:  FIX: Seems not to be working. Compare with Windows installation.
-;;;; - FIXaaa
-;;;; - TODOaaa
+;;;; Temp stuff for testing the above:
+;;;; - [FIXME]aaa
 ;;;; - FIXMEaaa
+;;;; - [FIX]aaa
+;;;; - FIXaaa
+;;;; - [TODO-THINK]aaa
+;;;; - TODO-THINKaaa
+;;;; - [TODO]aaa
+;;;; - TODOaaa
+;;;; - [HACK]aaa
 ;;;; - HACKaaa
+;;;; - [REFACTOR]aaa
 ;;;; - REFACTORaaa
+;;;; - [NOCOMMIT]aaa
+;;;; - NOCOMMITaaa
 ;;;; - [REMAINING-ISSUE]aaa
 ;;;; - REMAINING-ISSUEaaa
-;;;; - TODO-THINKaaa
 
 ;;;; ___________________________________________________________________________
 
