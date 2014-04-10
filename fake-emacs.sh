bf12c24396a364c0fce4003fa0544399cac83d1e
Simon Katz <sk@nomistech.com>
2014-04-10 09:37:02 +0100

    Extract variable `emacs_exec` in "fake-emacs.sh".

--- fake-emacs.sh
+++ fake-emacs.sh
@@ -1,11 +1,13 @@
 # Creating a scratch Emacs
 # - See Nic Ferrier's http://www.emacsbites.com/episode/scratch-emacs.
 
+# emacs_exec="emacs -nw"
+emacs_exec="open -n -a /Applications/Emacs-24-2-1.app --args"
+
 function fake-emacs {
     [ "$1" != "" ] || { echo "missing arg -- need an installation name" ; return 1 ; }
     emacsd=$1
     shift
     [ -d $emacsd ] || mkdir $emacsd
-    # HOME=$(realpath $emacsd) emacs -nw $*
-    HOME="`pwd`/$emacsd" open -n -a /Applications/Emacs-24-2-1.app --args $*
+    HOME="`pwd`/$emacsd" $emacs_exec $*
 }
