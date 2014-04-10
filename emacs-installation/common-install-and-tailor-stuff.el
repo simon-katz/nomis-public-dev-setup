5f949bd69749323cb64bf1875571af9a5707f237
Simon Katz <sk@nomistech.com>
2014-04-10 08:31:52 +0100

    Simplify Emacs version check logic.

--- emacs-installation/common-install-and-tailor-stuff.el
+++ emacs-installation/common-install-and-tailor-stuff.el
@@ -1,13 +1,14 @@
 (let ((expected-version "24.2.1")
       (version emacs-version))
-  (unless (equal version expected-version)
-    (unless (y-or-n-p (format (concat
-                               "Things might not work. This Emacs init is"
-                               " expecting Emacs %s, but this is Emacs %s."
-                               " Type 'y' to continue or 'n' to exit.")
-                              expected-version
-                              version))
-      (kill-emacs))))
+  (unless (or (equal version expected-version)
+              (y-or-n-p
+               (format (concat
+                        "Things might not work. This Emacs init is"
+                        " expecting Emacs %s, but this is Emacs %s."
+                        " Type 'y' to continue or 'n' to exit.")
+                       expected-version
+                       version)))
+    (kill-emacs)))
 
 (require 'package)
 
