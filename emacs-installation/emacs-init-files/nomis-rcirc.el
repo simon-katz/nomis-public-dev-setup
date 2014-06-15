81f7dbf3477c8825b31dc1f7e5acdf2e751dd0ef
Simon Katz <git@nomistech.com>
2014-06-15 14:35:41 +0100

    Add rcirc notifications.

--- emacs-installation/emacs-init-files/nomis-rcirc.el
+++ emacs-installation/emacs-init-files/nomis-rcirc.el
@@ -2,6 +2,16 @@
 
 (eval-after-load 'rcirc '(require 'rcirc-notify))
 
+(rcirc-notify-add-hooks)
+
+(setq rcirc-notify-timeout -1)
+
+(defun rcirc-notify-allowed (nick &optional delay)
+  ;; TODO: This is a gross hack.
+  ;;       But, actually, I think it gives me what I want, and I think
+  ;;       the tailoring options don't allow for this.
+  t)
+
 ;; (add-hook 'rcirc-mode-hook
 ;;           (lambda ()
 ;;             (rcirc-track-minor-mode 1)))
