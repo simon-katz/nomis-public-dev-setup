591f6122dc0b9eb28cc04be968c8b3b45d7804ae
Simon Katz <sk@nomistech.com>
2014-07-29 21:24:57 +0100

    Hacking of `clojure-indent-function` (which, BTW, is fucked).

--- emacs-installation/emacs-init-files/nomis-clojure-indentation.el
+++ emacs-installation/emacs-init-files/nomis-clojure-indentation.el
@@ -18,7 +18,103 @@
      (fact 'defun)
      (facts 'defun)
      (against-background 'defun)
-     (provided 0)))
+     (provided 0)
+     ;; Om
+     ;; (dom/li 1)
+     ;; (dom/ul 1)
+     ;; (dom/div 1)
+     ;; (dom/input 1)
+     ;; (dom/label 1)
+     ))
+
+
+(cond
+ ((member clojure-mode-version
+          '("2.1.0"))
+  (defun clojure-indent-function (indent-point state)
+    "This function is the normal value of the variable `lisp-indent-function'.
+It is used when indenting a line within a function call, to see if the
+called function says anything special about how to indent the line.
+
+INDENT-POINT is the position where the user typed TAB, or equivalent.
+Point is located at the point to indent under (for default indentation);
+STATE is the `parse-partial-sexp' state for that position.
+
+If the current line is in a call to a Lisp function
+which has a non-nil property `lisp-indent-function',
+that specifies how to do the indentation.  The property value can be
+* `defun', meaning indent `defun'-style;
+* an integer N, meaning indent the first N arguments specially
+  like ordinary function arguments and then indent any further
+  arguments like a body;
+* a function to call just as this function was called.
+  If that function returns nil, that means it doesn't specify
+  the indentation.
+
+This function also returns nil meaning don't specify the indentation."
+    (let ((normal-indent (current-column)))
+      (goto-char (1+ (elt state 1)))
+      (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
+      (if (and (elt state 2)
+               (not (looking-at "\\sw\\|\\s_")))
+          ;; car of form doesn't seem to be a symbol
+          (progn
+            (if (not (> (save-excursion (forward-line 1) (point))
+                        calculate-lisp-indent-last-sexp))
+                (progn (goto-char calculate-lisp-indent-last-sexp)
+                       (beginning-of-line)
+                       (parse-partial-sexp (point)
+                                           calculate-lisp-indent-last-sexp 0 t)))
+            ;; Indent under the list or under the first sexp on the same
+            ;; line as calculate-lisp-indent-last-sexp.  Note that first
+            ;; thing on that line has to be complete sexp since we are
+            ;; inside the innermost containing sexp.
+            (backward-prefix-chars)
+            (if (and (eq (char-after (point)) ?\[)
+                     (eq (char-after (elt state 1)) ?\())
+                (+ (current-column) 2) ;; this is probably inside a defn
+              (current-column)))
+        (let* ((function (buffer-substring (point)
+                                           (progn (forward-sexp 1) (point))))
+               (open-paren (elt state 1))
+               (method nil)
+               (function-name (substring-no-properties function))
+               (function-tail (first
+                               (last
+                                (split-string function-name "/")))))
+          (setq method
+                (or (get (intern-soft function-name) 'clojure-indent-function)
+                    (get (intern-soft function-tail) 'clojure-indent-function)
+                    (when (and (> (length function-name) 4)
+                               (equal (subseq function-name 0 4)
+                                      "dom/"))
+                      1)))
+
+          (cond ((member (char-after open-paren) '(?\[ ?\{))
+                 (goto-char open-paren)
+                 (1+ (current-column)))
+                ((or (eq method 'defun)
+                     (and clojure-defun-style-default-indent
+                          ;; largely to preserve useful alignment of :require, etc in ns
+                          (not (string-match "^:" function))
+                          (not method))
+                     (and (null method)
+                          (> (length function) 3)
+                          (string-match "\\`\\(?:\\S +/\\)?\\(def\\|with-\\)"
+                                        function)))
+                 (lisp-indent-defform state indent-point))
+
+                ((integerp method)
+                 (lisp-indent-specform method state
+                                       indent-point normal-indent))
+                (method
+                 (funcall method indent-point state))
+                (clojure-use-backtracking-indent
+                 (clojure-backtracking-indent
+                  indent-point state normal-indent))))))))
+ (t
+  (message-box
+   "You need to fix your clojure-indent-function for this version of clojure-mode.")))
 
 ;;;; ___________________________________________________________________________
 
