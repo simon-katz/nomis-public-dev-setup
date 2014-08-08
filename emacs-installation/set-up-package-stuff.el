073bc837a4b39e7f6eae237f747a5ea112662a82
Simon Katz <sk@nomistech.com>
2014-08-08 18:56:02 +0100

    Use melpa-stable as your package archive.

--- emacs-installation/set-up-package-stuff.el
+++ emacs-installation/set-up-package-stuff.el
@@ -1,11 +1,15 @@
 (require 'package)
 
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/")
-             t)
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "http://marmalade-repo.org/packages/")
+;;              t)
 
 ;; (add-to-list 'package-archives
 ;;              '("melpa" . "http://melpa.milkbox.net/packages/")
 ;;              t)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa-stable.milkbox.net/packages/")
+             t)
 
 (package-initialize)
