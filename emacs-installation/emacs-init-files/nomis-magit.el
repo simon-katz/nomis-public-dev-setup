d463800de074ec1dfae31bd61e9d6681cf4b4871
Simon Katz <sk@nomistech.com>
2015-05-24 23:28:42 +0100

    Nicer colours for magit diff.

--- emacs-installation/emacs-init-files/nomis-magit.el
+++ emacs-installation/emacs-init-files/nomis-magit.el
@@ -6,11 +6,11 @@
 
 (setq magit-completing-read-function 'magit-ido-completing-read)
 
-;; (eval-after-load 'magit
-;;   '(progn
-;;      (set-face-foreground 'magit-diff-add "green4")
-;;      (set-face-foreground 'magit-diff-del "orangered2")
-;;      (set-face-background 'magit-item-highlight "palegoldenrod")))
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green4")
+     (set-face-foreground 'magit-diff-del "orangered2")
+     (set-face-background 'magit-item-highlight "palegoldenrod")))
 
 (global-set-key (kbd "C-c g") 'magit-status)
 
