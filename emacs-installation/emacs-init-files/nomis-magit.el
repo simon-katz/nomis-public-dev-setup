835e30390981eae187012d1a609c6a0ba60708ea
Simon Katz <sk@nomistech.com>
2015-05-24 23:28:16 +0100

    Add whitespace toggle to Magit.

--- emacs-installation/emacs-init-files/nomis-magit.el
+++ emacs-installation/emacs-init-files/nomis-magit.el
@@ -16,4 +16,29 @@
 
 ;;;; ___________________________________________________________________________
 
+;;;; From http://whattheemacsd.com/setup-magit.el-02.html
+
+(eval-after-load 'magit
+  '(progn
+     
+     (defun magit-toggle-whitespace ()
+       (interactive)
+       (if (member "-w" magit-diff-options)
+           (magit-dont-ignore-whitespace)
+         (magit-ignore-whitespace)))
+
+     (defun magit-ignore-whitespace ()
+       (interactive)
+       (add-to-list 'magit-diff-options "-w")
+       (magit-refresh))
+
+     (defun magit-dont-ignore-whitespace ()
+       (interactive)
+       (setq magit-diff-options (remove "-w" magit-diff-options))
+       (magit-refresh))
+
+     (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)))
+
+;;;; ___________________________________________________________________________
+
 (provide 'nomis-magit)
