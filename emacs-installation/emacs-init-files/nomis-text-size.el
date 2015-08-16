a30e7353e516cd9e8ab53a8af2899ebbe1ce29ca
Simon Katz <sk@nomistech.com>
2015-08-16 13:35:08 +0100

    Change `nomis/font-resize` to have quit cancel functionality.

--- emacs-installation/emacs-init-files/nomis-text-size.el
+++ emacs-installation/emacs-init-files/nomis-text-size.el
@@ -5,9 +5,13 @@
 
 ;;;; Largely copied from http://emacs.stackexchange.com/questions/7583/transiently-adjust-text-size-in-mode-line-and-minibuffer/7584#7584 .
 
-(defvar nomis/initial-point-size (face-attribute 'default :height))
+(defun nomis/get-default-point-size ()
+  (face-attribute 'default :height))
 
-(defvar nomis/point-size nomis/initial-point-size)
+(defun nomis/set-default-point-size (size)
+  (set-face-attribute 'default nil :height size))
+
+(defvar nomis/default-default-point-size (nomis/get-default-point-size))
 
 (defun nomis/font-size-adjust (&optional arg)
   "Globally change the font size.
@@ -16,12 +20,11 @@ M-<NUM> M-x nomis/font-size-adjust:
 - decreases font size by (10% ^ NUM) if NUM is -ve
 - resets    font size if NUM is 0."
   (interactive "p")
-  (setq nomis/point-size
-        (if (= arg 0)
-            nomis/initial-point-size
-          (round (* nomis/point-size
-                    (expt 1.1 arg)))))
-  (set-face-attribute 'default nil :height nomis/point-size))
+  (let ((new-size (if (= arg 0)
+                      nomis/default-default-point-size
+                    (round (* (nomis/get-default-point-size)
+                              (expt 1.1 arg))))))
+    (nomis/set-default-point-size new-size)))
 
 (defun nomis/font-size-incr  () (interactive) (nomis/font-size-adjust +1)) 
 (defun nomis/font-size-decr  () (interactive) (nomis/font-size-adjust -1)) 
@@ -29,12 +32,42 @@ M-<NUM> M-x nomis/font-size-adjust:
 
 (require 'hydra)
 
-(defhydra nomis-font-resize
-  (global-map "M-+")
-  "font-resize"
-  ("-"   nomis/font-size-decr  "Decrease")
-  ("="   nomis/font-size-incr  "Increase")
-  ("0"   nomis/font-size-reset "Reset to default size"))
+(defvar nomis/font-resize-initial-size)
+
+(defun nomis/font-resize-init ()
+  (interactive)
+  (setq nomis/font-resize-initial-size (nomis/get-default-point-size)))
+
+(defun nomis/font-resize-cancel ()
+  (interactive)
+  (nomis/set-default-point-size nomis/font-resize-initial-size))
+
+(defun nomis/font-resize-quit ()
+  (interactive)
+  (nomis-no-op))
+
+(defvar nomis/font-resize-initialised-p nil)
+
+(defmacro define-nomis/font-resize (key)
+  ;; The hacking with `key` and binding it to `nomis/font-resize/body`
+  ;; is to get the hints to appear when the command is first activated.
+  `(progn
+     (defhydra nomis/font-resize
+       (global-map ,key
+                   :pre (unless nomis/font-resize-initialised-p
+                          (nomis/font-resize-init)
+                          (setq nomis/font-resize-initialised-p t))
+                   :post (setq nomis/font-resize-initialised-p nil))
+       "font-resize"
+       ("-"        nomis/font-size-decr     "Decrease")
+       ("="        nomis/font-size-incr     "Increase")
+       ("0"        nomis/font-size-reset    "Reset to default size")
+       ("<escape>" nomis/font-resize-cancel "Cancel" :exit t)
+       ("<return>" nomis/font-resize-quit   "Quit"   :exit t)
+       ("q"        nomis/font-resize-quit   "Quit"   :exit t))
+     (define-key global-map (kbd ,key) 'nomis/font-resize/body)))
+
+(define-nomis/font-resize "M-+")
 
 ;;;; ___________________________________________________________________________
 
