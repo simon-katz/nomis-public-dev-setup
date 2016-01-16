a20d77f0c5b18f0f1dbabcf468f0359545b73bf0
Simon Katz <sk@nomistech.com>
2016-01-16 16:01:38 +0000

    Delete some comments and commented-out code

--- emacs-installation/emacs-init-files/nomis-indent-sexp.el
+++ emacs-installation/emacs-init-files/nomis-indent-sexp.el
@@ -1,36 +1,7 @@
 ;;;; Init stuff -- Indenting s-expressions.
 
-;;;; TODO: Do you want this?
-;;;;       Is the default horrible?
-;;;;       Was there a Starter Kit issue that made you do this?
-
 ;;;; ___________________________________________________________________________
-;;;; ---- Want C-M-q to work when at the end of a sexp ----
-
-;; It's best to do this kind of thing (modifying an existing
-;; command) using advice.
-;; But don't delete the following because it has examples of modifying
-;; modes that you may want to refer to.
-
-;; (defun nomis-indent-sexp (arg)
-;;   (interactive "P")
-;;   (save-excursion
-;;     (flet ((do-it () (indent-pp-sexp arg)))
-;;       (if (or (looking-at "(")
-;;               (let ((just-after-close-paren-p
-;;                      (prog2
-;;                          (backward-char)
-;;                          (looking-at ")")
-;;                        (forward-char))))
-;;                 (not just-after-close-paren-p)))
-;;           (do-it)
-;;         (progn
-;;           (backward-sexp)
-;;           (do-it)
-;;           ;; (forward-sexp) ; not needed because of save-excursion
-;;           )))))
-
-;;;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+;;;; ---- Want indentation commands to work when at the end of a sexp ----
 
 (defadvice indent-pp-sexp (around work-when-at-end-of-sexp (&optional arg))
   (save-excursion
