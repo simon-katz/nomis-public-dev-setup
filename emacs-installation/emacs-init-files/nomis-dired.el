a1d053079b788d116cbdcb3ae9fe1d2318f05211
Simon Katz <sk@nomistech.com>
2017-08-07 21:08:39 +0100

    Only set `dired-listing-switches` when on OSX

--- emacs-installation/emacs-init-files/nomis-dired.el
+++ emacs-installation/emacs-init-files/nomis-dired.el
@@ -3,7 +3,10 @@
 ;;;; ___________________________________________________________________________
 ;;;; ---- Misc ----
 
-(setq dired-listing-switches "-Ao")
+(cond ((equal system-type 'darwin)
+       (setq dired-listing-switches "-Ao"))
+      ((equal system-type 'windows-nt)
+       (message-box "Check dired-listing-switches for windows-nt")))
 
 (require 'nomis-dired-explorer)
 
