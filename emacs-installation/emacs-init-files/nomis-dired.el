5031183c1cdec599f7355859703fb45c6c4e036c
Simon Katz <sk@nomistech.com>
2017-08-20 12:15:56 +0100

    Improve dired setup

--- emacs-installation/emacs-init-files/nomis-dired.el
+++ emacs-installation/emacs-init-files/nomis-dired.el
@@ -3,10 +3,23 @@
 ;;;; ___________________________________________________________________________
 ;;;; ---- Misc ----
 
-(cond ((equal system-type 'darwin)
-       (setq dired-listing-switches "-Ao"))
-      ((equal system-type 'windows-nt)
-       (message-box "Check dired-listing-switches for windows-nt")))
+(setq dired-listing-switches
+      (concatenate 'string
+                   "-"
+                   "a" ; show dotfiles
+                   "o" ; like -l but without group information
+                   "h" ; human-readable
+                   ))
+
+(when (equal system-type 'darwin)
+  (let ((gls "/usr/local/bin/gls"))
+    (when (file-exists-p gls)
+      (setq insert-directory-program gls))))
+
+(when (equal system-type 'windows-nt)
+  (message-box "Check dired setup for windows-nt"))
+
+;;;; ___________________________________________________________________________
 
 (require 'nomis-dired-explorer)
 
