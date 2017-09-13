bb9f8da11b8a97c7560087486adc71aa540f2562
Simon Katz <sk@nomistech.com>
2017-09-13 13:35:47 +0100

    Add `nomis/pairs->list`

--- emacs-installation/emacs-init-files/manually-installed/nomis-idle-highlight-mode-tests.el
+++ emacs-installation/emacs-init-files/manually-installed/nomis-idle-highlight-mode-tests.el
@@ -16,6 +16,22 @@ position in the buffer)."
   (number-sequence 1
                    (1+ (length string))))
 
+(defmacro nomis/pairs->list (&rest pairs)
+  "Create a list as specified by PAIRS.
+Let the pairs be (N-1 Obj-1), (N-2 Obj-2), etc.
+Each Ni is a non-negative integer.
+Each Obj-i is any object.
+The result is the concatenation of:
+     N-1 copies of Obj-1
+     N-2 copies of Obj-2
+     etc."
+  `(funcall 'concatenate
+            'list
+            ,@(mapcar (lambda (pair)
+                        `(make-list ,(first pair)
+                                    ,(second pair)))
+                      pairs)))
+
 (defun nomis/with-temp-buffer-etc (mode-fun string position fun)
   (with-temp-buffer
     (funcall mode-fun)
@@ -42,14 +58,14 @@ position in the buffer)."
 
 (ert-deftest niht/basic-test ()
   (should (equal (mapcar 'niht/run-basic-test
-                 '(nil nil
-                   "foo" "foo" "foo" "foo"
-                   nil
-                   "bar" "bar" "bar" "bar"
-                   nil
-                   "baz" "baz" "baz" "baz"
-                   nil nil nil))))
                          (nomis/string->all-positions niht/basic-test/string))
+                 (nomis/pairs->list (2 nil)
+                                    (4 "foo")
+                                    (1 nil)
+                                    (4 "bar")
+                                    (1 nil)
+                                    (4 "baz")
+                                    (3 nil)))))
 
 ;;;; ___________________________________________________________________________
 
