3a7fa3a7bd49a6f5a1094609d9d44bffb7971b5d
Simon Katz <sk@nomistech.com>
2017-09-17 19:45:09 +0100

    Fix screw up: Delete Clojure reader comment stuff from "nomis-cider-extras.el"
    You will delete "nomis-nrepl-extras.el" in a minute so you don't do this kind of
    thing again. (You replaced that years ago with "nomis-cider-extras.el".)

--- emacs-installation/emacs-init-files/nomis-cider-extras.el
+++ emacs-installation/emacs-init-files/nomis-cider-extras.el
@@ -396,47 +396,6 @@ Really send to REPL? "
 (define-key clojure-mode-map (kbd "C-c C-g")
   'nomis-cider-rearrange-string-into-lines)
 
-;;;; ___________________________________________________________________________
-;;;; ---- Reader comments ----
-
-;;;; Inspired by https://gist.github.com/4349847
-;;;; ...which says...
-;;;;     inspired by http://bc.tech.coop/blog/070122.html
-;;;;     ported from slime/contrib/slime-editing-commands.el
-
-(define-key clojure-mode-map (kbd "C-c ;")
-  'nomis-cider-insert-reader-comment)
-(define-key clojure-mode-map (kbd "C-c M-;")
-  'nomis-cider-remove-reader-comment)
-
-(defun nomis-cider-insert-reader-comment (prefix)
-  "Insert a reader comment (#_) around the s-expression containing the point.
-If this command is invoked repeatedly (without any other command
-occurring between invocations), the comment progressively moves outward
-over enclosing expressions. If invoked with a positive prefix argument,
-the s-expression prefix expressions out is enclosed in a set of balanced
-comments."
-  (interactive "*p")
-  (save-excursion
-    (if (eq last-command this-command)
-        (when (search-backward "#_" nil t) ; wrong -- not structure-aware
-          (delete-char 2)
-          (backward-up-list)
-          (insert "#_"))
-      (progn
-        (nomis-move-to-start-of-bracketed-sexp-around-point)
-        (dotimes (i (1- prefix))
-          (backward-up-list)
-          (decf prefix))
-        (insert "#_")))))
-
-(defun nomis-cider-remove-reader-comment ()
-  "Remove a reader comment enclosing point."
-  (interactive "*")
-  (save-excursion
-    (when (search-backward "#_" nil t) ; wrong -- not structure-aware
-      (delete-char 2))))
-
 ;;## ;;;; ___________________________________________________________________________
 ;;## 
 ;;## ;;;; TODO: Tidy; just hacked for now.
