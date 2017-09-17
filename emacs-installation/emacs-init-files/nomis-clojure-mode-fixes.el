889cd372b19c45b90efb3d745dc4cc404ccd1f83
Simon Katz <sk@nomistech.com>
2017-09-17 18:38:53 +0100

    Create "nomis-clojure-mode-fixes.el" and move stuff to there

---
+++ emacs-installation/emacs-init-files/nomis-clojure-mode-fixes.el
@@ -0,0 +1,26 @@
+;;;; Init stuff -- Fix bugs in clojure-mode.
+
+;;;; ___________________________________________________________________________
+
+(require 'nomis-sexp-utils)
+
+(when (equal clojure-mode-version "5.6.1")
+  ;; Fix broken `clojure-cycle-privacy`
+  (defun clojure-cycle-privacy ()
+    "Make public the current private def, or vice-versa.
+See: https://github.com/clojure-emacs/clj-refactor.el/wiki/cljr-cycle-privacy"
+    (interactive)
+    (save-excursion
+      (nomis-beginning-of-this-defun)
+      (search-forward-regexp "(defn?\\(-\\| ^:private\\)?\\_>")
+      (if (match-string 1)
+          (replace-match "" nil nil nil 1)
+        (goto-char (match-end 0))
+        (insert (if (or clojure-use-metadata-for-privacy
+                        (equal (match-string 0) "(def"))
+                    " ^:private"
+                  "-"))))))
+
+;;;; ___________________________________________________________________________
+
+(provide 'nomis-clojure-mode-fixes)
--- emacs-installation/emacs-init-files/nomis-clojure.el
+++ emacs-installation/emacs-init-files/nomis-clojure.el
@@ -4,31 +4,7 @@
 ;;;; clojure-mode
 
 (require 'clojure-mode)
-
-;;;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-;;;; Fix broken things
-
-(require 'nomis-sexp-utils)
-
-(when (equal clojure-mode-version "5.6.1")
-  ;; Fix broken `clojure-cycle-privacy`
-  (defun clojure-cycle-privacy ()
-    "Make public the current private def, or vice-versa.
-See: https://github.com/clojure-emacs/clj-refactor.el/wiki/cljr-cycle-privacy"
-    (interactive)
-    (save-excursion
-      (nomis-beginning-of-this-defun)
-      (search-forward-regexp "(defn?\\(-\\| ^:private\\)?\\_>")
-      (if (match-string 1)
-          (replace-match "" nil nil nil 1)
-        (goto-char (match-end 0))
-        (insert (if (or clojure-use-metadata-for-privacy
-                        (equal (match-string 0) "(def"))
-                    " ^:private"
-                  "-"))))))
-
-;;;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-;;;; Tailor
+(require 'nomis-clojure-mode-fixes)
 
 (setq clojure-use-metadata-for-privacy t)
 
