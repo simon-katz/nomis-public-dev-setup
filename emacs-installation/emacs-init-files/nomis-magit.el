e97e288dd3337658dd2fcf6904bad05baf8e918d
Simon Katz <sk@nomistech.com>
2017-09-23 15:06:14 +0100

    Turn off Magit's broken auto-revert stuff

--- emacs-installation/emacs-init-files/nomis-magit.el
+++ emacs-installation/emacs-init-files/nomis-magit.el
@@ -19,13 +19,25 @@
         '(t "%Y-%m-%d %H:%M:%S" magit-log-margin-width t 18)) 
   (add-to-list 'same-window-regexps "\*magit: .*\*") ; was: (setq magit-status-buffer-switch-function 'switch-to-buffer) -- no longer works
   (setq magit-completing-read-function 'magit-ido-completing-read)
-  ;; (setq magit-revert-buffers 'silent) obsolete
+  (progn ; Old auto-revert stuff
+    ;; (setq magit-revert-buffers 'silent) obsolete
+    )
+  (progn ; New auto-revert stuff
+    ;; Magit now sets up auto-revert-mode on buffers it knows about (or something).
+    ;; Sheesh!
+    ;; See https://emacs.stackexchange.com/questions/35701/magit-sets-auto-revert-mode-annoying
+    (with-eval-after-load 'magit-autorevert
+      (when (equal magit-version "2.10.3") ; FIXME Move this to a "fixes" file
+        ;; I want reverting only when I do a Magit operation, but that has been
+        ;; broken. Better to have auto-revert completely off.
+        (magit-auto-revert-mode 0)
+        ;; Maybe good to revert buffers when Magit makes changes:
+        (defalias 'magit-auto-revert-buffers 'revert-all-unmodified-buffers))))
   ;; (setq magit-push-always-verify nil) ; no longer exists
   (setq magit-diff-refine-hunk 'all)
   (setq magit-diff-highlight-trailing nil)
   ;; (setq git-commit-summary-max-length 999)
   )
-
 
 (add-hook 'magit-mode-hook 'nomis-init-magit)
 
