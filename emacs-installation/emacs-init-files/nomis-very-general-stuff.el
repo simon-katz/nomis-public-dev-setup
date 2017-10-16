f30e957995a501d582857d33c39660d60df39a69
Simon Katz <sk@nomistech.com>
2017-10-16 01:05:09 +0100

    Make `visual-line-fringe-indicators` less intrusive

--- emacs-installation/emacs-init-files/nomis-very-general-stuff.el
+++ emacs-installation/emacs-init-files/nomis-very-general-stuff.el
@@ -28,9 +28,31 @@
 (setq sentence-end-double-space nil)
 
 (show-paren-mode 1)
+
+
 (setq line-move-visual nil) ; the default of T is annoying, and it
                             ; screws up keyboard macros
-(setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
+(define-fringe-bitmap 'nomis/right-curly-arrow
+  [#b00000000
+   #b00000000
+   #b00000000
+   #b00000000
+   #b01110000
+   #b00010000
+   #b00010000
+   #b00000000])
+(define-fringe-bitmap 'nomis/left-curly-arrow
+  [#b00000000
+   #b00001000
+   #b00001000
+   #b00001110
+   #b00000000
+   #b00000000
+   #b00000000
+   #b00000000])
+(setq visual-line-fringe-indicators '(nomis/left-curly-arrow
+                                      nomis/right-curly-arrow))
+
 
 (setq nomis-backup-directory (expand-file-name "~/.emacs-backups/"))
 (make-directory nomis-backup-directory t)
