7be710c548b0d5dfdceccdd2f489d7183afc3941
Simon Katz <sk@nomistech.com>
2018-03-11 14:15:57 +0000

    Add `nomis/find-window-in-any-frame`

--- emacs-installation/emacs-init-files/nomis-buffers-windows-frames.el
+++ emacs-installation/emacs-init-files/nomis-buffers-windows-frames.el
@@ -4,13 +4,20 @@
 
 ;;;; ___________________________________________________________________________
 
-(defun nomis/find-window-in-frame (buffer-name)
-  (let* ((frame (selected-frame))
+(cl-defun nomis/find-window-in-frame (buffer-name &optional frame)
+  (let* ((frame (or frame
+                    (selected-frame)))
          (windows (window-list frame)))
     (cl-find-if (lambda (w)
                   (equal (-> w window-buffer buffer-name)
                          buffer-name))
                 windows)))
+
+(defun nomis/find-window-in-any-frame (buffer-name)
+  (loop for f in (frame-list)
+        for w = (nomis/find-window-in-frame buffer-name f)
+        when w
+        return w))
 
 ;;;; ___________________________________________________________________________
 
