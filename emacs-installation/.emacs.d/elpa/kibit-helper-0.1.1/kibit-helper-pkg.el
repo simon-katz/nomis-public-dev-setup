da21b857cb0951c2bda2a968db4474b46a56a4e5
Simon Katz <sk@nomistech.com>
2018-03-30 23:46:42 +0100

    Add kibit-helper

---
+++ emacs-installation/.emacs.d/elpa/kibit-helper-0.1.1/kibit-helper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kibit-helper" "0.1.1" "Conveniently use the Kibit Leiningen plugin from Emacs" '((s "0.8") (emacs "24")) :commit "ec5f154db3bb0c838e86f527353f08644cede926" :url "http://www.github.com/brunchboy/kibit-helper" :keywords '("languages" "clojure" "kibit"))
