ff2e72009e819dad8887889f5e124b6ec32e76b8
Simon Katz <sk@nomistech.com>
2018-08-19 13:31:20 +0100

    Replace `nomis/right-margin-mode` with `nomis/cycle-80-column-stuff` etc

--- emacs-installation/emacs-init-files/nomis-right-margin.el
+++ emacs-installation/emacs-init-files/nomis-right-margin.el
@@ -8,41 +8,72 @@
 (require 'nomis-fci-mode)
 (require 'column-marker)
 
-(defface nomis/column-marker-1 '((t (:background "magenta")))
+(defface nomis/column-marker-1-face '((t (:background "magenta")))
   "Face used for a column marker.  Usually a background color."
   :group 'faces)
 
-(setq column-marker-1-face 'nomis/column-marker-1)
-
-(define-minor-mode nomis/right-margin-mode
-  "A minor mode that combines whitespace-mode and fci-mode."
-  nil nil nil
-  (if nomis/right-margin-mode
-      (progn
-        ;; (whitespace-mode)
-        ;; (fci-mode) ; See `nomis/fci-mode-issues`.
-        (column-marker-1 nomis/right-margin-column))
-    (progn
-      ;; (whitespace-mode-off)
-      ;; (fci-mode-off) ; See `nomis/fci-mode-issues`.
-      (column-marker-1 '(4)))))
-
-(defun turn-on-nomis/right-margin-mode ()
-  "Turn on nomis/right-margin-mode unconditionally."
-  (interactive)
-  (nomis/right-margin-mode 1))
-
-(defun turn-off-nomis/right-margin-mode ()
-  "Turn off nomis/right-margin-mode unconditionally."
-  (interactive)
-  (nomis/right-margin-mode 0))
+(setq column-marker-1-face 'nomis/column-marker-1-face)
 
 ;;;; ___________________________________________________________________________
 
-(add-hook 'text-mode-hook 'turn-on-nomis/right-margin-mode)
-(add-hook 'prog-mode-hook 'turn-on-nomis/right-margin-mode)
+(defun nomis/get-80-column-stuff ()
+  (let* ((w? whitespace-mode)
+         (c? column-marker-1))
+    (cond ((and (not w?) (not c?)) 0)
+          ((and (not w?)      c?)  1)
+          ((and      w?  (not c?)) 2)
+          (t                       3))))
 
-(add-hook 'org-mode-hook 'turn-off-nomis/right-margin-mode)
+(defun nomis/set-80-column-stuff (n)
+  (interactive "p")
+  (message "nomis/set-80-column-stuff setting approach to %s" n)
+  (cl-flet ((w-off () (whitespace-mode 0))
+            (w-on  () (whitespace-mode 1))
+            (c-off () (column-marker-1 '(4)))
+            (c-on  () (column-marker-1 nomis/right-margin-column)))
+    (case n
+      (0 (w-off) (c-off))
+      (1 (w-off) (c-on))
+      (2 (w-on)  (c-off))
+      (3 (w-on)  (c-on)))))
+
+(defun nomis/set-80-column-stuff-0 ()
+  (interactive)
+  (nomis/set-80-column-stuff 0))
+
+(defun nomis/set-80-column-stuff-1 ()
+  (interactive)
+  (nomis/set-80-column-stuff 1))
+
+(defun nomis/set-80-column-stuff-2 ()
+  (interactive)
+  (nomis/set-80-column-stuff 2))
+
+(defun nomis/set-80-column-stuff-3 ()
+  (interactive)
+  (nomis/set-80-column-stuff 3))
+
+(defun nomis/cycle-80-column-stuff ()
+  (interactive)
+  (nomis/set-80-column-stuff (mod (1+ (nomis/get-80-column-stuff))
+                                  4)))
+
+;;;; ___________________________________________________________________________
+
+(defun nomis/set-80-column-stuff-1-after-delay ()
+  ;; If you do this without a delay, the column-marker-1 stuff gets blatted
+  ;; by the whitespace-mode stuff. (I guess something must run after this.)
+  (run-at-time "1 sec" nil (lambda () (nomis/set-80-column-stuff-1))))
+
+(defun nomis/set-80-column-stuff-3-after-delay ()
+  ;; If you do this without a delay, the column-marker-1 stuff gets blatted
+  ;; by the whitespace-mode stuff. (I guess something must run after this.)
+  (run-at-time "1 sec" nil (lambda () (nomis/set-80-column-stuff-3))))
+
+(add-hook 'text-mode-hook 'nomis/set-80-column-stuff-3-after-delay)
+(add-hook 'prog-mode-hook 'nomis/set-80-column-stuff-3-after-delay)
+
+(add-hook 'org-mode-hook 'nomis/set-80-column-stuff-1-after-delay)
 
 ;;;; ___________________________________________________________________________
 
