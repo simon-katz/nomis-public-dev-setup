486fc6804780984e0b3297dfb72736c4a3c3bbdd
Simon Katz <sk@nomistech.com>
2018-08-21 14:46:12 +0100

    Make highlight-indentation-mode colours less intrusive

--- emacs-installation/emacs-init-files/nomis-highlight-indentation.el
+++ emacs-installation/emacs-init-files/nomis-highlight-indentation.el
@@ -2,11 +2,11 @@
 
 (defun nomis/setup-highlight-indentation-mode ()
   ;; From https://github.com/antonj/Highlight-Indentation-for-Emacs
-  (set-face-background 'highlight-indentation-face "#e3e3d3"))
+  (set-face-background 'highlight-indentation-face "grey90"))
 
 (defun nomis/setup-highlight-indentation-current-column-mode ()
   ;; From https://github.com/antonj/Highlight-Indentation-for-Emacs
-  (set-face-background 'highlight-indentation-current-column-face "#c3b3b3"))
+  (set-face-background 'highlight-indentation-current-column-face "grey60"))
 
 (add-hook 'highlight-indentation-mode-hook
           'nomis/setup-highlight-indentation-mode)
