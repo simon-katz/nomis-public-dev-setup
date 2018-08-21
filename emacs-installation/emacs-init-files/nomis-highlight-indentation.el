7b26002e32bd2d5170ce1a19435e0d5346d77a1d
Simon Katz <sk@nomistech.com>
2018-08-21 23:16:43 +0100

    Delete no-longer-needed `nomis/toggle-fold-indentation`

--- emacs-installation/emacs-init-files/nomis-highlight-indentation.el
+++ emacs-installation/emacs-init-files/nomis-highlight-indentation.el
@@ -14,21 +14,4 @@
 (add-hook 'highlight-indentation-current-column-mode-hook
           'nomis/setup-highlight-indentation-current-column-mode)
 
-(defun nomis/toggle-fold-indentation ()
-  ;; From https://stackoverflow.com/questions/1587972/how-to-display-indentation-guides-in-emacs/4459159#4459159
-  ;; (and renamed).
-  "Toggle fold all lines larger than indentation on current line"
-  (interactive)
-  (let ((col 1))
-    (save-excursion
-      (back-to-indentation)
-      (setq col (+ 1 (current-column)))
-      (set-selective-display
-       (if selective-display nil (or col 1))))))
-
-;;;; TODO You have integrated this with `nomis/hs-toggle-hiding`.
-;;;;      Can you add more functionality here, so that you can hide/show
-;;;;      a single chunk rather than having the same level across a whole
-;;;;      buffer?
-
 (provide 'nomis-highlight-indentation)
