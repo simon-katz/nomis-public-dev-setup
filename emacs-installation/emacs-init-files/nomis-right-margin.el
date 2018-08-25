77efdbe68ce3f1718943d602bb2d6e87232e7ab7
Simon Katz <sk@nomistech.com>
2018-08-25 15:43:17 +0100

    Make 80-column-stuff do things to fci-mode instead of whitespace-mode

--- emacs-installation/emacs-init-files/nomis-right-margin.el
+++ emacs-installation/emacs-init-files/nomis-right-margin.el
@@ -3,11 +3,11 @@
 ;;;; ___________________________________________________________________________
 
 (require 'nomis-right-margin-column)
-(require 'nomis-whitespace)
+(require 'nomis-fci-mode)
 (require 'column-marker)
 
 (defface nomis/column-marker-1-face `((t :box ,(list :line-width -1
-                                                     :color "red"
+                                                     :color fci-rule-color ; "red"
                                                      :style nil)))
   "Face used for a column marker.  Usually a background color."
   :group 'faces)
@@ -17,25 +17,25 @@
 ;;;; ___________________________________________________________________________
 
 (defun nomis/get-80-column-stuff ()
-  (let* ((w? whitespace-mode)
+  (let* ((f? fci-mode)
          (c? column-marker-1))
-    (cond ((and (not w?) (not c?)) 0)
-          ((and (not w?)      c?)  1)
-          ((and      w?  (not c?)) 2)
+    (cond ((and (not f?) (not c?)) 0)
+          ((and (not f?)      c?)  1)
+          ((and      f?  (not c?)) 2)
           (t                       3))))
 
 (defun nomis/set-80-column-stuff (n)
   (interactive "p")
   (message "nomis/set-80-column-stuff setting approach to %s" n)
-  (cl-flet ((w-off () (whitespace-mode 0))
-            (w-on  () (whitespace-mode 1))
+  (cl-flet ((f-off () (fci-mode 0))
+            (f-on  () (fci-mode 1))
             (c-off () (column-marker-1 '(4)))
             (c-on  () (column-marker-1 nomis/right-margin-column)))
     (case n
-      (0 (w-off) (c-off))
-      (1 (w-off) (c-on))
-      (2 (w-on)  (c-off))
-      (3 (w-on)  (c-on)))))
+      (0 (f-off) (c-off))
+      (1 (f-off) (c-on))
+      (2 (f-on)  (c-off))
+      (3 (f-on)  (c-on)))))
 
 (defun nomis/set-80-column-stuff-0 ()
   (interactive)
