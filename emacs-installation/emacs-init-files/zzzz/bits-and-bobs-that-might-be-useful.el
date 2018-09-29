ad56a7b00acc56aa67497caa39b06c560a9290af
Simon Katz <sk@nomistech.com>
2018-09-29 20:01:39 +0100

    Add zzzz/bits-and-bobs-that-might-be-useful.el

---
+++ emacs-installation/emacs-init-files/zzzz/bits-and-bobs-that-might-be-useful.el
@@ -0,0 +1,23 @@
+;;;; ___________________________________________________________________________
+;;;; From nomis-dirtee
+
+(defun zzzz-nomis/dirtree/remove-trailing-slash (s)
+  (replace-regexp-in-string "/\\'" "" s))
+
+(defun zzzz-nomis/dirtree/full-filename->list-from-file-system-root (full-filename)
+  (assert (eq ?/ (string-to-char full-filename)))
+  (->> (substring full-filename 1)
+       zzzz-nomis/dirtree/remove-trailing-slash
+       (s-split "/")))
+
+(progn ; tests
+  (let ((strings '("/aa/bbb/cccc/"
+                   "/aa/bbb/cccc")))
+    (assert (equal (-map #'zzzz-nomis/dirtree/remove-trailing-slash
+                         strings)
+                   '("/aa/bbb/cccc"
+                     "/aa/bbb/cccc")))
+    (assert (equal (-map #'zzzz-nomis/dirtree/full-filename->list-from-file-system-root
+                         strings)
+                   '(("aa" "bbb" "cccc")
+                     ("aa" "bbb" "cccc"))))))
