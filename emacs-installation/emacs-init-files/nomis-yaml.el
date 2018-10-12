cebf7d0075d26d6dff1d14e12e724755d809278f
Simon Katz <simon.katz@youview.com>
2018-10-12 10:24:20 +0100

    yaml-mode: Set up whitespace and 80-column stuff

--- emacs-installation/emacs-init-files/nomis-yaml.el
+++ emacs-installation/emacs-init-files/nomis-yaml.el
@@ -5,7 +5,8 @@
 
 (defun nomis/setup-yaml-mode ()
   ;; (define-key yaml-mode-map "\C-m" 'newline-and-indent)
-  )
+  (nomis/set-whitespace-value-2)
+  (nomis/set-80-column-stuff-0))
 
 (add-hook 'yaml-mode-hook 'nomis/setup-yaml-mode)
 (add-hook 'yaml-mode-hook 'highlight-indentation-mode)
