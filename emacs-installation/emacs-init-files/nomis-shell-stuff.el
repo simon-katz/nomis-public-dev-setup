2fda1fb105ebd21004e03d4a9b7743f01d6884f1
Simon Katz <sk@nomistech.com>
2019-01-26 23:46:05 +0000

    Add highlight-indentation-mode to sh-mode

--- emacs-installation/emacs-init-files/nomis-shell-stuff.el
+++ emacs-installation/emacs-init-files/nomis-shell-stuff.el
@@ -1,8 +1,14 @@
 ;;;; Init stuff -- Shell stuff.
 
+;;;; ___________________________________________________________________________
+;;;; Style
 
 (add-hook 'sh-mode-hook 'flycheck-mode)
+(add-hook 'sh-mode-hook 'highlight-indentation-mode)
 
+;; (setq sh-basic-offset 4) ; Leave at the default
+
+;;;; ___________________________________________________________________________
 
 (defun shell-region (start end)
   ;; Copied from http://stackoverflow.com/questions/6286579/emacs-shell-mode-how-to-send-region-to-shell.
