4b158063750afb69cdaf6f37d02bf18aa711ca97
Simon Katz <sk@nomistech.com>
2019-02-02 19:12:11 +0000

    Turn on `global-undo-tree-mode` and `undo-tree-auto-save-history`

--- emacs-installation/emacs-init-files/nomis-undo-tree.el
+++ emacs-installation/emacs-init-files/nomis-undo-tree.el
@@ -2,13 +2,15 @@
 
 (require 'undo-tree)
 
-;; (global-undo-tree-mode)
+(global-undo-tree-mode)
+
+(setq undo-tree-auto-save-history t)
 
 (progn
   ;; The default for M-z is zap-to-char.  I don't need that, so...
 
   ;; Hah! This is the thing that causes you to lose undo history!
-  
+
   ;; (define-key global-map (kbd "M-z") 'undo-tree-undo)
   ;; (define-key global-map (kbd "M-Z") 'undo-tree-redo)
   )
