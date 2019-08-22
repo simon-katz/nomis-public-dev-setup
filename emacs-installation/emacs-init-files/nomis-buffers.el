579723d1b1079b81aacb4b104a81e1e4a62d6547
Simon Katz <sk@nomistech.com>
2019-08-22 14:04:47 +0100

    Refactor `nomis/report-point-etc`

--- emacs-installation/emacs-init-files/nomis-buffers.el
+++ emacs-installation/emacs-init-files/nomis-buffers.el
@@ -2,11 +2,14 @@
 
 ;;;; ___________________________________________________________________________
 
+(defun nomis/point-etc-string ()
+  (format "(point) = %s of %s"
+          (point)
+          (point-max)))
+
 (defun nomis/report-point-etc ()
   (interactive)
-  (message "(point) = %s of %s"
-           (point)
-           (point-max)))
+  (message (nomis/point-etc-string)))
 
 (define-key global-map (kbd "C-c =") 'nomis/report-point-etc)
 
