3bb2196257acd36ac346eb79c117496b68a24815
Simon Katz <sk@nomistech.com>
2019-09-01 00:09:25 +0100

    Improve nomis-scrolling by not letting visual-line-mode interfere

--- emacs-installation/emacs-init-files/nomis-scrolling.el
+++ emacs-installation/emacs-init-files/nomis-scrolling.el
@@ -2,10 +2,23 @@
 
 ;;;; ___________________________________________________________________________
 
+(defun with-no-visual-line-mode* (fun)
+  (let* ((visual-line-mode-was-on? visual-line-mode))
+    (when visual-line-mode-was-on?
+      (visual-line-mode 0))
+    (unwind-protect
+        (funcall fun)
+      (when visual-line-mode-was-on?
+        (visual-line-mode 1)))))
+
+(cl-defmacro with-no-visual-line-mode (&body body)
+  `(with-no-visual-line-mode* (lambda () ,@body)))
+
 (defun -nomis/with-maintain-line-no-in-window* (fun)
-  (let* ((line-no-in-window (count-screen-lines (window-start) (point) t)))
-    (prog1 (funcall fun)
-      (recenter (1- line-no-in-window)))))
+  (with-no-visual-line-mode
+   (let* ((line-no-in-window (count-screen-lines (window-start) (point) t)))
+     (prog1 (funcall fun)
+       (recenter (1- line-no-in-window))))))
 
 (cl-defmacro nomis/with-maintain-line-no-in-window (&body body)
   (declare (indent 0))
