70fed51f66c4b6978d50772d934931f8449c480b
Simon Katz <sk@nomistech.com>
2019-09-05 14:41:59 +0100

    Display error popups with a pink background

--- emacs-installation/emacs-init-files/nomis-popup.el
+++ emacs-installation/emacs-init-files/nomis-popup.el
@@ -5,7 +5,6 @@
 ;;;; ___________________________________________________________________________
 ;;;; ____ * Require things
 
-(require 'nomis-msg) ; TODO Get rid of use of `nomis/msg/grab-user-attention/low` and use a pink colour for the popup instead
 (require 'nomis-scrolling)
 
 ;;;; ___________________________________________________________________________
@@ -19,15 +18,21 @@
   `((t (:foreground "black" :background ,nomis/popup/muted-yellow)))
   "Face used for popups.")
 
+(defvar nomis/popup/pink "pink")
+
+(defface nomis/popup/error-face
+  `((t (:foreground "black" :background ,nomis/popup/pink)))
+  "Face used for popups.")
+
 ;;;; ___________________________________________________________________________
 ;;;; ____ * nomis/popup/message
 
 (defvar -nomis/popup/most-recent-popup-time nil)
 
-(defun -make-nomis-popup-overlay (start-pos end-pos &rest props)
+(defun -make-nomis-popup-overlay (face start-pos end-pos &rest props)
   (let* ((ov (make-overlay start-pos end-pos)))
     (overlay-put ov 'category 'nomis-popup)
-    (overlay-put ov 'face     'nomis/popup/face)
+    (overlay-put ov 'face     face)
     (while props (overlay-put ov (pop props) (pop props)))
     ov))
 
@@ -49,7 +54,7 @@ If POS is nil, use `point' instead."
 
 (add-hook 'pre-command-hook '-nomis/popup/remove-existing-popups/force)
 
-(defun nomis/popup/message (format-string &rest args)
+(defun -nomis/popup/message* (face format-string &rest args)
   (cl-flet ((n-chars-we-can-replace-at-pos
              (pos)
              (let* ((n-chars-before-eol
@@ -76,7 +81,8 @@ If POS is nil, use `point' instead."
            (ov1-start-pos popup-pos)
            (ov2-start-pos (+ popup-pos msg-part-1-len)))
       (let* ((msg-part-1 (substring msg 0 msg-part-1-len))
-             (ov1 (-make-nomis-popup-overlay ov1-start-pos
+             (ov1 (-make-nomis-popup-overlay face
+                                             ov1-start-pos
                                              ov2-start-pos
                                              'display  msg-part-1))))
       (let* ((msg-part-2 (substring msg msg-part-1-len)))
@@ -84,9 +90,10 @@ If POS is nil, use `point' instead."
           (put-text-property 0
                              (length msg-part-2)
                              'face
-                             'nomis/popup/face
+                             face
                              msg-part-2)
-          (let* ((ov2 (-make-nomis-popup-overlay ov2-start-pos
+          (let* ((ov2 (-make-nomis-popup-overlay face
+                                                 ov2-start-pos
                                                  ov2-start-pos
                                                  'before-string msg-part-2))))))
       (setq -nomis/popup/most-recent-popup-time (float-time))
@@ -98,14 +105,20 @@ If POS is nil, use `point' instead."
                          (with-current-buffer buffer
                            (-nomis/popup/remove-existing-popups)))))))))
 
+(defun nomis/popup/message (format-string &rest args)
+  (apply #'-nomis/popup/message*
+         'nomis/popup/face
+         format-string
+         args))
+
 (defvar nomis/popup/error-message-prefix "!! ")
 
 (defun nomis/popup/error-message (format-string &rest args)
-  (apply #'nomis/popup/message
+  (apply #'-nomis/popup/message*
+         'nomis/popup/error-face
          (concat nomis/popup/error-message-prefix
                  format-string)
-         args)
-  (nomis/msg/grab-user-attention/low))
+         args))
 
 ;;;; ___________________________________________________________________________
 ;;;; * End
