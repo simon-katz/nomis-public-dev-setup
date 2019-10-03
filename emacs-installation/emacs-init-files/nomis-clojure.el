d6817e889f55eb2ecc541d6497931171398886e0
Simon Katz <sk@nomistech.com>
2019-10-03 13:40:15 +0100

    Use a relative file name for `cider-repl-history-file`

--- emacs-installation/emacs-init-files/nomis-clojure.el
+++ emacs-installation/emacs-init-files/nomis-clojure.el
@@ -41,12 +41,24 @@
 
 
 (progn
-  ;; In projects, you can create a directory-local variable for
-  ;; `cider-repl-history-file` in `cider-repl-mode`, as follows:
-  ;; - create a file called ".dir-locals.el" with this content:
-  ;;     ((cider-repl-mode
-  ;;       (cider-repl-history-file . ".cider-repl-history")))
-  (setq cider-repl-history-file "~/.cider-repl-history")
+  (progn ; set `cider-repl-history-file`
+    ;; Old notes:
+    ;;   In projects, you can create a directory-local variable for
+    ;;   `cider-repl-history-file` in `cider-repl-mode`, as follows:
+    ;;   - create a file called ".dir-locals.el" with this content:
+    ;;       ((cider-repl-mode
+    ;;         (cider-repl-history-file . ".cider-repl-history")))
+    ;; New thoughts on 2019-10-03:
+    ;; - If you use ".cider-repl-history" rather than "~/.cider-repl-history"
+    ;;   all seems OK, despite confusing (perhaps outdated) comments about using
+    ;;   dir locals at the following places:
+    ;;   http://blogish.nomistech.com/separate-cider-repl-histories/
+    ;;   https://github.com/clojure-emacs/cider/issues/2353#issuecomment-401126801
+    ;;   https://github.com/clojure-emacs/cider/issues/37#issuecomment-7312083
+    ;; - So not this:
+    ;;   (setq cider-repl-history-file "~/.cider-repl-history")
+    ;; But this:
+    (setq cider-repl-history-file ".cider-repl-history"))
   (setq cider-repl-history-size 5000) ; the default is 500
   )
 
