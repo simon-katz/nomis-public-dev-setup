e8baa55e58e575b41c55c937d7afab4928217481
Simon Katz <sk@nomistech.com>
2019-11-03 20:25:07 +0000

    Get rid of pop-up help in Emacs Lisp, because it's broken
    It creates a second window for no good reason.
    It doesn't work if there are two windows showing different buffers.
    Not sure of behaviour when there are more than two buffers.

--- emacs-installation/emacs-init-files/nomis-emacs-lisp-and-ielm.el
+++ emacs-installation/emacs-init-files/nomis-emacs-lisp-and-ielm.el
@@ -19,45 +19,6 @@
 
 (define-key emacs-lisp-mode-map (kbd "RET") 'newline-and-indent)
 
-;;;; ___________________________________________________________________________
-;;;; Pop-up help in Emacs Lisp.
-
-(require 'popup)
-
-(defun nomis/describe-thing-in-popup ()
-  ;; Thanks to Bruce Durling, Kris Jenkins and Steve Purcell.
-  ;; Based on code taken from
-  ;; https://github.com/otfrom/otfrom-org-emacs/blob/master/org/config.org.
-  ;; See also http://blog.jenkster.com/2013/12/popup-help-in-emacs-lisp.html.
-  (interactive)
-  (let* ((thing (symbol-at-point))
-         (help-xref-following t)
-         (description (with-temp-buffer
-                        (help-mode)
-                        (help-xref-interned thing)
-                        (buffer-string))))
-    ;; Provide a useful message, and overwrite the now-wrong message about
-    ;; typing "q" in the help window.
-    (message "Type \"C-g\" to dismiss popup.")
-    (popup-tip description
-               :point (point)
-               :around t
-               :height 30
-               :scroll-bar t
-               :margin t)))
-
-(progn
-  ;; Key binding.
-  ;;
-  ;; elisp-slime-nav-mode binds the following keys
-  ;;   C-c C-d d
-  ;;   C-c C-d C-d
-  ;; to `elisp-slime-nav-describe-elisp-thing-at-point`, which displays
-  ;; documentation in the *Help* buffer.
-  ;; Here rebind only C-c C-d C-d, because it's useful to have both.
-  (require 'elisp-slime-nav)
-  (define-key elisp-slime-nav-mode-map (kbd "C-c C-d C-d") 'nomis/describe-thing-in-popup))
-
 ;;; ___________________________________________________________________________
 
 (provide 'nomis-emacs-lisp-and-ielm)
