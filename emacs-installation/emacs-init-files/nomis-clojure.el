4327d086ac1b4c75cabd907eb30fc2963b5d1b12
Simon Katz <sk@nomistech.com>
2019-12-10 14:43:50 +0000

    Move `flycheck-error-format-message-and-id` advice to nomis-flycheck

--- emacs-installation/emacs-init-files/nomis-clojure.el
+++ emacs-installation/emacs-init-files/nomis-clojure.el
@@ -38,40 +38,6 @@
 ;;                     (clj-kondo-edn . edn-joker)))
 ;;   (flycheck-add-next-checker (car checkers) (cons 'error (cdr checkers))))
 
-(progn ; Include checker name in flycheck messages in echo area.
-  (cond
-   ((member (flycheck-version)
-            '("31"))
-    (defvar *-nomis/add-checker-name-to-flycheck-message?* nil)
-    (advice-add 'flycheck-display-error-messages
-                :around
-                (lambda (orig-fun &rest args)
-                  (let* ((*-nomis/add-checker-name-to-flycheck-message?* t))
-                    (apply orig-fun args)))
-                '((name . nomis/add-checker-name-to-flycheck-message*)))
-    (advice-add 'flycheck-error-format-message-and-id
-                :around
-                (lambda (orig-fun err)
-                  (let* ((raw-value (funcall orig-fun err)))
-                    (if *-nomis/add-checker-name-to-flycheck-message?*
-                        (let* ((error-checker-info
-                                (concat "("
-                                        (symbol-name (flycheck-error-checker err))
-                                        ")")))
-                          (put-text-property 0
-                                             (length error-checker-info)
-                                             'face
-                                             '(foreground-color . "blue")
-                                             error-checker-info)
-                          (concat error-checker-info
-                                  " "
-                                  raw-value))
-                      raw-value)))
-                '((name . nomis/add-checker-name-to-flycheck-message*))))
-   (t
-    (message-box
-     "You need to fix our 'include checker name in flycheck messages' for this version of flycheck."))))
-
 
 ;;;; ___________________________________________________________________________
 ;;;; Cider
--- emacs-installation/emacs-init-files/nomis-flycheck.el
+++ emacs-installation/emacs-init-files/nomis-flycheck.el
@@ -10,4 +10,41 @@
 
 ;;;; ___________________________________________________________________________
 
+(progn ; Include checker name in flycheck messages in echo area.
+  (cond
+   ((member (flycheck-version)
+            '("31"
+              "32snapshot (package: 20191126.2142)"))
+    (defvar *-nomis/add-checker-name-to-flycheck-message?* nil)
+    (advice-add 'flycheck-display-error-messages
+                :around
+                (lambda (orig-fun &rest args)
+                  (let* ((*-nomis/add-checker-name-to-flycheck-message?* t))
+                    (apply orig-fun args)))
+                '((name . nomis/add-checker-name-to-flycheck-message*)))
+    (advice-add 'flycheck-error-format-message-and-id
+                :around
+                (lambda (orig-fun err)
+                  (let* ((raw-value (funcall orig-fun err)))
+                    (if *-nomis/add-checker-name-to-flycheck-message?*
+                        (let* ((error-checker-info
+                                (concat "("
+                                        (symbol-name (flycheck-error-checker err))
+                                        ")")))
+                          (put-text-property 0
+                                             (length error-checker-info)
+                                             'face
+                                             '(foreground-color . "blue")
+                                             error-checker-info)
+                          (concat error-checker-info
+                                  " "
+                                  raw-value))
+                      raw-value)))
+                '((name . nomis/add-checker-name-to-flycheck-message*))))
+   (t
+    (message-box
+     "You need to fix our 'include checker name in flycheck messages' for this version of flycheck."))))
+
+;;;; ___________________________________________________________________________
+
 (provide 'nomis-flycheck)
