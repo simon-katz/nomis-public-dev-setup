4434bc9d7f0c37eb1616fd6d6accd77b0e521187
Simon Katz <sk@nomistech.com>
2020-07-10 19:55:38 +0100

    Add Flycheck for Python

--- emacs-installation/emacs-init-files/nomis-python.el
+++ emacs-installation/emacs-init-files/nomis-python.el
@@ -5,5 +5,12 @@
 (elpy-enable)
 
 ;;;; ___________________________________________________________________________
+;;;; Flycheck
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;;;; ___________________________________________________________________________
 
 (provide 'nomis-python)
