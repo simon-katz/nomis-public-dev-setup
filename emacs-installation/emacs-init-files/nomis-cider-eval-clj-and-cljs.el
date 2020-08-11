c9d73aad11769e04216cfc33fe80910f82e65dd2
Simon Katz <sk@nomistech.com>
2020-08-11 17:35:42 +0100

    Turn off evaluating both clj and cljs at the same time
    Not sure this was doing The Right Thing.

--- emacs-installation/emacs-init-files/nomis-cider-eval-clj-and-cljs.el
+++ emacs-installation/emacs-init-files/nomis-cider-eval-clj-and-cljs.el
@@ -79,101 +79,103 @@
 
 ;;;; ___________________________________________________________________________
 
-(defvar *nomis/cider/eval-clj-and-cljs/feature-on? t)
+;;;; Turn this off for now -- I'm not convinced I understood things properly. eg what is a sibling connection?
 
-(cond ; nomis/eval-clj-and-cljs
- ;; For example, send forms to both a CLJ REPL and a CLJS REPL from a
- ;; .cljc file.
+;; (defvar *nomis/cider/eval-clj-and-cljs/feature-on? nil)
 
- ;; Take care to send forms to each REPL process only once, so deal with
- ;; removing duplicates when there are siblings.
+;; (cond ; nomis/eval-clj-and-cljs
+;;  ;; For example, send forms to both a CLJ REPL and a CLJS REPL from a
+;;  ;; .cljc file.
 
- ;; Do this by hacking `cider-repls` in some call chains to not use
- ;; `sesman-current-session` -- instead, start by calling
- ;; `sesman-current-sessions` and then process the return value.
+;;  ;; Take care to send forms to each REPL process only once, so deal with
+;;  ;; removing duplicates when there are siblings.
 
- ;; TODO: Consider simply changing `cider-repls` (and not just in particular
- ;;       call chains).
- ;;       BUT your recent changes (one REPL of each type per session) mean you
- ;;       have moved on a long way, so maybe not. Maybe a new function to be
- ;;       called from a few places.
+;;  ;; Do this by hacking `cider-repls` in some call chains to not use
+;;  ;; `sesman-current-session` -- instead, start by calling
+;;  ;; `sesman-current-sessions` and then process the return value.
 
- ;; TODO: `cider-repl-switch-to-other` is broken. When there are sibling REPLs
- ;;       of the same type, it can go to one of them. The doc string says switch
- ;;       between CLJ and CLJS.
+;;  ;; TODO: Consider simply changing `cider-repls` (and not just in particular
+;;  ;;       call chains).
+;;  ;;       BUT your recent changes (one REPL of each type per session) mean you
+;;  ;;       have moved on a long way, so maybe not. Maybe a new function to be
+;;  ;;       called from a few places.
 
- ((member (cider-version)
-          '("CIDER 0.23.0 (Lima)"
-            "CIDER 0.24.0snapshot"))
+;;  ;; TODO: `cider-repl-switch-to-other` is broken. When there are sibling REPLs
+;;  ;;       of the same type, it can go to one of them. The doc string says switch
+;;  ;;       between CLJ and CLJS.
 
-  (defun -nomis/cider-repls-to-evaluate-in (&optional type ensure)
-    "Return cider REPLs of type TYPE from all current sessions.
-When there are sibling REPLs of the same type, include only one
-of the REPLs. If TYPE is nil or multi, return all repls. If TYPE
-is a list of types, return only REPLs of type contained in the
-list. If ENSURE is non-nil, throw an error if no linked session
-exists."
-    (let* ((type (cond
-                  ((listp type)
-                   (mapcar #'cider-maybe-intern type))
-                  ((cider-maybe-intern type))))
-           (sesman-sessions (sesman-current-sessions 'CIDER))
-           (one-repl-of-each-type-for-each-session
-            (mapcar (lambda (sesman-session)
-                      (let ((repls (cdr sesman-session)))
-                        (seq-uniq repls
-                                  (lambda (x y)
-                                    (eq (cider-repl-type x)
-                                        (cider-repl-type y))))))
-                    sesman-sessions))
-           (repls (apply #'append one-repl-of-each-type-for-each-session)))
-      (or (seq-filter (lambda (b)
-                        (cider--match-repl-type type b))
-                      repls)
-          (when ensure
-            (cider--no-repls-user-error type)))))
+;;  ((member (cider-version)
+;;           '("CIDER 0.23.0 (Lima)"
+;;             "CIDER 0.24.0snapshot"))
 
-  (defvar *-nomis/cider/eval-clj-and-cljs/hack-cider-repls? nil)
+;;   (defun -nomis/cider-repls-to-evaluate-in (&optional type ensure)
+;;     "Return cider REPLs of type TYPE from all current sessions.
+;; When there are sibling REPLs of the same type, include only one
+;; of the REPLs. If TYPE is nil or multi, return all repls. If TYPE
+;; is a list of types, return only REPLs of type contained in the
+;; list. If ENSURE is non-nil, throw an error if no linked session
+;; exists."
+;;     (let* ((type (cond
+;;                   ((listp type)
+;;                    (mapcar #'cider-maybe-intern type))
+;;                   ((cider-maybe-intern type))))
+;;            (sesman-sessions (sesman-current-sessions 'CIDER))
+;;            (one-repl-of-each-type-for-each-session
+;;             (mapcar (lambda (sesman-session)
+;;                       (let ((repls (cdr sesman-session)))
+;;                         (seq-uniq repls
+;;                                   (lambda (x y)
+;;                                     (eq (cider-repl-type x)
+;;                                         (cider-repl-type y))))))
+;;                     sesman-sessions))
+;;            (repls (apply #'append one-repl-of-each-type-for-each-session)))
+;;       (or (seq-filter (lambda (b)
+;;                         (cider--match-repl-type type b))
+;;                       repls)
+;;           (when ensure
+;;             (cider--no-repls-user-error type)))))
 
-  (advice-add 'cider-interactive-eval
-              :around
-              (lambda (orig-fun &rest args)
-                (let* ((*-nomis/cider/eval-clj-and-cljs/hack-cider-repls?
-                        *nomis/cider/eval-clj-and-cljs/feature-on?))
-                  (apply orig-fun args)))
-              '((name . nomis/eval-clj-and-cljs)
-                (depth . -100)))
+;;   (defvar *-nomis/cider/eval-clj-and-cljs/hack-cider-repls? nil)
 
-  (advice-add 'cider-load-buffer
-              :around
-              (lambda (orig-fun &rest args)
-                (let* ((*-nomis/cider/eval-clj-and-cljs/hack-cider-repls?
-                        *nomis/cider/eval-clj-and-cljs/feature-on?))
-                  (apply orig-fun args)))
-              '((name . nomis/eval-clj-and-cljs)
-                (depth . -100)))
+;;   (advice-add 'cider-interactive-eval
+;;               :around
+;;               (lambda (orig-fun &rest args)
+;;                 (let* ((*-nomis/cider/eval-clj-and-cljs/hack-cider-repls?
+;;                         *nomis/cider/eval-clj-and-cljs/feature-on?))
+;;                   (apply orig-fun args)))
+;;               '((name . nomis/eval-clj-and-cljs)
+;;                 (depth . -100)))
 
-  (advice-add 'cider-repls
-              :around
-              (lambda (orig-fun &rest args)
-                (if *-nomis/cider/eval-clj-and-cljs/hack-cider-repls?
-                    (apply #'-nomis/cider-repls-to-evaluate-in args)
-                  (apply orig-fun args)))
-              '((name . nomis/eval-clj-and-cljs)))
+;;   (advice-add 'cider-load-buffer
+;;               :around
+;;               (lambda (orig-fun &rest args)
+;;                 (let* ((*-nomis/cider/eval-clj-and-cljs/hack-cider-repls?
+;;                         *nomis/cider/eval-clj-and-cljs/feature-on?))
+;;                   (apply orig-fun args)))
+;;               '((name . nomis/eval-clj-and-cljs)
+;;                 (depth . -100)))
 
-  )
- (t
-  (message-box
-   "You need to fix `nomis/eval-clj-and-cljs` for this version of Cider.")))
+;;   (advice-add 'cider-repls
+;;               :around
+;;               (lambda (orig-fun &rest args)
+;;                 (if *-nomis/cider/eval-clj-and-cljs/hack-cider-repls?
+;;                     (apply #'-nomis/cider-repls-to-evaluate-in args)
+;;                   (apply orig-fun args)))
+;;               '((name . nomis/eval-clj-and-cljs)))
 
-;;;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+;;   )
+;;  (t
+;;   (message-box
+;;    "You need to fix `nomis/eval-clj-and-cljs` for this version of Cider.")))
 
-(when nil ; Code to remove advice when in dev.
-  (progn
-    (advice-remove 'cider-interactive-eval 'nomis/eval-clj-and-cljs)
-    (advice-remove 'cider-load-buffer      'nomis/eval-clj-and-cljs)
-    (advice-remove 'cider-repls            'nomis/eval-clj-and-cljs))
-  )
+;; ;;;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+;; (when nil ; Code to remove advice when in dev.
+;;   (progn
+;;     (advice-remove 'cider-interactive-eval 'nomis/eval-clj-and-cljs)
+;;     (advice-remove 'cider-load-buffer      'nomis/eval-clj-and-cljs)
+;;     (advice-remove 'cider-repls            'nomis/eval-clj-and-cljs))
+;;   )
 
 ;;;; ___________________________________________________________________________
 
