b46fc94855e7645e9d080a1c72b44fb96c8b076d
Simon Katz <sk@nomistech.com>
2020-08-20 12:33:51 +0100

    Add `nomis/ret-for-y` advice to `y-or-n-p`

--- emacs-installation/emacs-init-files/nomis-y-or-n-p.el
+++ emacs-installation/emacs-init-files/nomis-y-or-n-p.el
@@ -13,4 +13,19 @@
 
 ;;;; ___________________________________________________________________________
 
+(defvar nomis/y-or-n-p-query-replace-map-with-ret-for-y
+  (let* ((m (copy-keymap query-replace-map)))
+    (define-key m (kbd "RET") 'act)
+    m))
+
+(advice-add
+ 'y-or-n-p
+ :around
+ (lambda (orig-func &rest args)
+   (let* ((query-replace-map nomis/y-or-n-p-query-replace-map-with-ret-for-y))
+     (apply orig-func args)))
+ '((name . nomis/ret-for-y)))
+
+;;;; ___________________________________________________________________________
+
 (provide 'nomis-y-or-n-p)
