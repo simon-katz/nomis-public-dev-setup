eb55a4c50e8a9aee9286c29f08b1f02942bb477f
Simon Katz <sk@nomistech.com>
2020-10-27 20:08:24 +0000

    Move `nomis/cider-rearrange-string-into-lines` to have lexical binding
    Needed for what we will do in the next commit.

--- emacs-installation/emacs-init-files/nomis-cider-extras-2.el
+++ emacs-installation/emacs-init-files/nomis-cider-extras-2.el
@@ -50,6 +50,54 @@
 
 (pushnew "deftest-msg" cider-test-defining-forms) ; a Wefarm Nabu thing
 
+;;## ;;;; ___________________________________________________________________________
+;;## ;;;; ---- nomis/cider-rearrange-string-into-lines ----
+;;##
+;;## ;;;; ****
+;;## ;;;; + Ensure `nomis/grab-text' has no free variables.
+;;## ;;;;
+;;## ;;;; + Put all your code-manipulation Clojure functions in single file in
+;;## ;;;;   a new project.
+;;## ;;;;   And have proper tests of the code-manipulation code.
+;;## ;;;;
+
+;;## (defun get-string-from-file (filePath)
+;;##   "Return FILEPATH's file content."
+;;##   ;; http://xahlee.blogspot.co.uk/2010/09/elisp-read-file-content-in-one-shot.html
+;;##   ;; which says:
+;;##   ;;   thanks to “Pascal J Bourguignon”
+;;##   ;;   and "TheFlyingDutchman <zzbba...@aol.com>". 2010-09-02
+;;##   ;;
+;;##   ;; I changed insert-file-contents to insert-file-contents-literally
+;;##   (with-temp-buffer
+;;##     (insert-file-contents-literally filePath)
+;;##     (buffer-string)))
+
+(defun nomis/cider-rearrange-string-into-lines (prefix)
+  "Rearrange string into lines.
+   Without a prefix argument, indent second and subsequent lines so
+   that they line up sensibly with the first line.
+   With a prefix argument, indent second and subsequent lines one
+   character less as is the convention for Clojure doc strings
+   (which is stupid)."
+  (interactive "*P")
+  (if (y-or-n-p "Use `fill-paragraph` instead?")
+      (fill-paragraph)
+    (let* ((string (nomis/grab-text
+                    :top-level-p nil
+                    :delete-p t))
+           (clojure-form-as-string
+            (format "(do (require '[com.nomistech.emacs-hacks-in-clojure :as ehic])
+                         (ehic/rearrange-string-into-lines '%s %s %s))"
+                    string
+                    (+ (current-column)
+                       (if prefix 0 1))
+                    72)))
+      (nomis/run-clojure-and-insert-result clojure-form-as-string))))
+
+(define-key cider-mode-map (kbd "C-c C-g")
+  'nomis/cider-rearrange-string-into-lines)
+
 ;;;; ___________________________________________________________________________
 
 (provide 'nomis-cider-extras-2)
