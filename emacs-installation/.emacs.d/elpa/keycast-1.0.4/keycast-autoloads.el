e973a9854bd9fe17349fecd3d859673d36c2fc22
Simon Katz <sk@nomistech.com>
2020-11-17 21:23:57 +0000

    Install keycast-mode

---
+++ emacs-installation/.emacs.d/elpa/keycast-1.0.4/keycast-autoloads.el
@@ -0,0 +1,37 @@
+;;; keycast-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "keycast" "keycast.el" (0 0 0 0))
+;;; Generated autoloads from keycast.el
+
+(defvar keycast-mode nil "\
+Non-nil if Keycast mode is enabled.
+See the `keycast-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `keycast-mode'.")
+
+(custom-autoload 'keycast-mode "keycast" nil)
+
+(autoload 'keycast-mode "keycast" "\
+Show current command and its key binding in the mode line.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "keycast" '("keycast-" "mode-line-keycast")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; keycast-autoloads.el ends here
---
+++ emacs-installation/.emacs.d/elpa/keycast-1.0.4/keycast-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "keycast" "1.0.4" "Show current command and its key in the mode line" '((emacs "25.3")) :commit "16d9961d15536054632be1eff75fd0fb1a4420f8" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/tarsius/keycast")
