524a0607f28a0aa329db562fbf3dd61ac50c94a1
Simon Katz <sk@nomistech.com>
2021-02-25 19:14:42 +0000

    Add auto-revert tailoring

--- emacs-installation/dot-emacs.el
+++ emacs-installation/dot-emacs.el
@@ -244,6 +244,8 @@
 
 (require 'nomis-flycheck)
 
+(require 'nomis-auto-revert)
+
 (progn
   ;; Putting this where it belongs (in "nomis/very-general-stuff") doesn't work;
   ;; I guess something blats it.
