d4a304302a62b13c21ed0dc84cb0bb58c2afe0e0
Simon Katz <sk@nomistech.com>
2021-02-27 13:09:55 +0000

    Add `logview` tailoring: Don't wrap lines

--- emacs-installation/emacs-init-files/nomis-logview.el
+++ emacs-installation/emacs-init-files/nomis-logview.el
@@ -3,6 +3,7 @@
 (require 'logview)
 
 ;;;; ___________________________________________________________________________
+;;;; ---- Globals ----
 
 (setq logview-auto-revert-mode 'auto-revert-tail-mode)
 
@@ -12,6 +13,18 @@
            (java-pattern . "yyyy-MM-dd HH:mm:ss,SSS"))
          logview-additional-timestamp-formats
          :test 'equal)
+
+;;;; ___________________________________________________________________________
+;;;; ---- Per-buffer ----
+
+(defun -nomis/logview-setup ()
+  ;; Elsewhere (eg in `nomis/org-mode`) we're using `visual-line-mode`, but
+  ;; that doesn't work here.  So use `truncate-lines` (which I guess we'd
+  ;; decided against before).
+  (setq truncate-lines t))
+
+(add-hook 'logview-mode-hook
+          '-nomis/logview-setup)
 
 ;;;; ___________________________________________________________________________
 
