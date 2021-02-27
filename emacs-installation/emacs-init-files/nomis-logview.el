95af43584db2f94ce308f4c9b80dc8e820a73558
Simon Katz <sk@nomistech.com>
2021-02-27 13:07:47 +0000

    Add `logview` tailoring: Comma between seconds and milliseconds

--- emacs-installation/emacs-init-files/nomis-logview.el
+++ emacs-installation/emacs-init-files/nomis-logview.el
@@ -8,6 +8,11 @@
 
 (setq logview-show-ellipses nil)
 
+(pushnew '("nomis-extra: ISO 8601 datetime + millis using comma"
+           (java-pattern . "yyyy-MM-dd HH:mm:ss,SSS"))
+         logview-additional-timestamp-formats
+         :test 'equal)
+
 ;;;; ___________________________________________________________________________
 
 (provide 'nomis-logview)
