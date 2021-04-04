89bd1e90949f2f48c4d23c969c366c53e8a28ea0
Simon Katz <sk@nomistech.com>
2021-04-04 19:25:33 +0100

    Prepare a bit for publishing `nomis-popup`

--- emacs-installation/emacs-init-files/nomis-popup.el
+++ emacs-installation/emacs-init-files/nomis-popup.el
@@ -1,4 +1,36 @@
-;;;; nomis-popup --- A layer on top of popup  ---  -*- lexical-binding: t -*-
+;;; nomis-popup.el --- Pop up messages using overlays -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019-2021 Simon Katz.
+
+;; Author: Simon Katz
+;; Version: 0.0.1-SNAPSHOT
+;; TODO: Add a contact details (or, probably, a GitHub URL).
+;; TODO: Add Keywords
+;; TODO: Add Package-Requires
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; TODO: When we make a repo for this, include the GNU General Public License
+;;       mentioned above.
+
+;;; Commentary:
+
+;; Pop up messages using overlays.
+
+;; TODO: Write webby documentation and add a URL here.
+
+;;; Code:
 
 (progn) ; this stops `hs-hide-all` from hiding the next comment
 
@@ -173,3 +205,4 @@ If POS is nil, use `point' instead."
 ;;;; * End
 
 (provide 'nomis-popup)
+;;; nomis-popup.el ends here
