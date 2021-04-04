f222a7f6355d7877abafa7e6390c0a5bc107963e
Simon Katz <sk@nomistech.com>
2021-04-04 20:25:11 +0100

    Prepare a bit for publishing `nomis-scrolling`

--- emacs-installation/emacs-init-files/nomis-scrolling.el
+++ emacs-installation/emacs-init-files/nomis-scrolling.el
@@ -1,4 +1,36 @@
-;;;; nomis-scrolling.el --- Scrolling ---  -*- lexical-binding: t -*-
+;;; nomis-scrolling.el --- Scrolling hacks  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019-2021 Simon Katz.
+
+;; Author: Simon Katz
+;; Version: 0.0.1-SNAPSHOT
+;; TODO: Add a contact details (or, probably, a GitHub URL).
+;; TODO: Add Keywords
+;; TODO: Add Package-Requires
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; TODO: When we make a repo for this, include the GNU General Public License
+;;       mentioned above.
+
+;;; Commentary:
+
+;; Scrolling hacks.
+
+;; TODO: Write webby documentation and add a URL here.
+
+;;; Code:
 
 ;;;; ___________________________________________________________________________
 
@@ -48,3 +80,4 @@
 ;;;; ___________________________________________________________________________
 
 (provide 'nomis-scrolling)
+;;; nomis-scrolling.el ends here
