c281627b094824d86dac849bc9f3e1823eeddfd4
Simon Katz <sk@nomistech.com>
2021-04-07 18:36:52 +0100

    Add `end-of-visual-line` to .mc-lists.el

--- emacs-installation/.emacs.d/.mc-lists.el
+++ emacs-installation/.emacs.d/.mc-lists.el
@@ -16,6 +16,7 @@
         delete-indentation
         down-list
         electric-newline-and-maybe-indent
+        end-of-visual-line
         eval-last-sexp
         forward-sentence
         forward-sexp
