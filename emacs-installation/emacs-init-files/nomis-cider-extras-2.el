460405d233fcae23c2d778d028fd3615ddf53671
Simon Katz <sk@nomistech.com>
2021-06-30 16:09:49 +0100

    Hack `cider-ns-refresh` to popup log buffer in the current window
    Also add extra logging before and after the refresh.

--- emacs-installation/emacs-init-files/nomis-cider-extras-2.el
+++ emacs-installation/emacs-init-files/nomis-cider-extras-2.el
@@ -48,6 +48,113 @@
 
 ;;;; ___________________________________________________________________________
 
+;;;; I don't understand why, but the messages displayed in the echo area by
+;;;; `cider-ns-refresh--handle-response` often disappear after a short time.
+;;;; -- Maybe this only happens when you define one or both of
+;;;;    `cider-ns-refresh-before-fn` and `cider-ns-refresh-after-fn`, as you do
+;;;;    in `nomis-kafka-clj-examples`.
+;;;;    -- Nope, that doesn't seem to be it.
+;;;;
+;;;; So, I've set `cider-ns-refresh-show-log-buffer`. But that doesn't select the
+;;;; log buffer, so we hack `cider-popup-buffer-display` to fix that. And also
+;;;; make sure that we disregard any window in another frame that is showing the
+;;;; log buffer.
+;;;;
+;;;; -- jsk 2021-06-28
+
+(defvar nomis/-cider-ns-refresh-count 0)
+
+(defun nomis/-cider-ns-refresh-log-pre-message ()
+  (s-join
+   "\n"
+   (list (format "%s----------------------------------------"
+                 (if (= 1 nomis/-cider-ns-refresh-count)
+                     ""
+                   "\n\n"))
+         (format ">>>> Doing cider-ns-refresh #%s"
+                 nomis/-cider-ns-refresh-count)
+         "")))
+
+(defun nomis/-cider-ns-refresh-log-post-message ()
+  (format
+   "<<<< Done cider-ns-refresh #%s\nPress \"q\" to exit"
+   nomis/-cider-ns-refresh-count))
+
+(cond
+ ((member (nomis/cider-version)
+          '("CIDER 0.26.1 (Nesebar)"))
+
+  (defun nomis/-get-cider-ns-refresh-log-buffer ()
+    (let* (;; Copied from `cider-ns-refresh`:
+           (existing-log-buffer (get-buffer cider-ns-refresh-log-buffer))
+           (log-buffer (or existing-log-buffer
+                           (cider-make-popup-buffer cider-ns-refresh-log-buffer))))
+      (when (null existing-log-buffer)
+        (with-current-buffer log-buffer
+          (unless truncate-lines
+            (toggle-truncate-lines))))
+      log-buffer))
+
+  (defvar *nomis/-hacking-cider-ns-refresh nil)
+
+  (advice-add
+   'cider-ns-refresh
+   :around
+   (lambda (orig-fun &rest args)
+     (incf nomis/-cider-ns-refresh-count)
+     (let* ((log-buffer (nomis/-get-cider-ns-refresh-log-buffer))
+            (msg (nomis/-cider-ns-refresh-log-pre-message)))
+       (cider-emit-into-popup-buffer log-buffer
+                                     msg
+                                     'font-lock-string-face
+                                     t))
+     (let* ((*nomis/-hacking-cider-ns-refresh t))
+       (apply orig-fun args)))
+   '((name . nomis/in-cider-ns-refresh)))
+
+  (advice-add
+   'cider-popup-buffer-display
+   :around
+   (lambda (orig-fun buffer &rest other-args)
+     (if *nomis/-hacking-cider-ns-refresh
+         ;; Delay this, because we mustn't change the current buffer for
+         ;; the code that is running -- people can use a .dir-locals.el
+         ;; to define buffer-local variables like
+         ;; `cider-ns-refresh-after-fn`.
+         ;; (It took a while for me to suss this out when things weren't
+         ;; working.)
+         (run-at-time 0
+                      nil
+                      (lambda ()
+                        (display-buffer-same-window buffer nil)))
+       (apply orig-fun buffer other-args)))
+   `((name . nomis/cider-popup-buffer-display/pop-to-buffer)))
+
+  (advice-add
+   'cider-ns-refresh--handle-response
+   :after
+   (lambda (response &rest other-args)
+     (nrepl-dbind-response response (status)
+       (when (member "invoked-after" status)
+         (run-at-time ; so user sees that something happened, even if it was quick
+          1
+          nil
+          (lambda ()
+            (let* ((log-buffer (nomis/-get-cider-ns-refresh-log-buffer))
+                   (msg (nomis/-cider-ns-refresh-log-post-message)))
+              (cider-emit-into-popup-buffer log-buffer
+                                            msg
+                                            'font-lock-string-face
+                                            t)))))))
+   '((name . nomis/in-cider-ns-refresh))))
+ (t
+  (message-box
+   "You need to fix `cider-popup-buffer-display for this version of CIDER.")))
+
+;; (advice-remove 'cider-popup-buffer-display 'nomis/cider-popup-buffer-display/pop-to-buffer)
+
+;;;; ___________________________________________________________________________
+
 (pushnew "deftest-msg" cider-test-defining-forms) ; a Wefarm Nabu thing
 
 ;;;; ___________________________________________________________________________
