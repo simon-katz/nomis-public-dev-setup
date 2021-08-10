9b10c05de973dc008881ecf9cf2f719884992641
Simon Katz <sk@nomistech.com>
2021-08-10 20:01:26 +0100

    Begin tailoring `lsp`

--- emacs-installation/emacs-init-files/nomis-clojure-lsp.el
+++ emacs-installation/emacs-init-files/nomis-clojure-lsp.el
@@ -4,6 +4,30 @@
 
 (setq lsp-keymap-prefix "H-q H-s")
 
+(defun nomis/lsp-init ()
+  (setq
+   lsp-lens-enable                  t
+   lsp-enable-symbol-highlighting   nil ; I prefer `nomis/idle-highlight`.
+   lsp-ui-doc-enable                nil ; Horrible big grey doc boxes.
+   lsp-enable-indentation           nil ; Use CIDER indentation.
+   ;; lsp-completion-enable            nil ; Uncomment to use Use CIDER completion
+   ;; gc-cons-threshold (* 100 1024 1024)
+   ;; read-process-output-max (* 1024 1024)
+   ;; treemacs-space-between-root-nodes nil
+   ;; lsp-signature-auto-activate      nil ; TODO: What is this?
+   ;; lsp-enable-dap-auto-configure    nil ; => t
+   ;; lsp-enable-file-watchers         nil ; => t
+   ;; lsp-enable-folding               nil ; => t
+   ;; lsp-enable-imenu                 nil ; => t
+   ;; lsp-enable-links                 nil ; => t
+   ;; lsp-enable-on-type-formatting    nil ; => t
+   ;; lsp-enable-snippet               nil ; => t
+   ;; lsp-enable-text-document-color   nil ; => t
+   ;; lsp-enable-xref                  nil ; => t
+   ))
+
+(add-hook 'lsp-mode-hook 'nomis/lsp-init)
+
 (add-hook 'clojure-mode-hook 'lsp)
 (add-hook 'clojurescript-mode-hook 'lsp)
 (add-hook 'clojurec-mode-hook 'lsp)
