6b16ee58b96b45289effcd8aebaae5f36d3b4a45
Simon Katz <sk@nomistech.com>
2021-08-10 18:34:38 +0100

    Upgrade to flycheck-clj-kondo-0.0.3

---
+++ emacs-installation/.emacs.d/elpa/flycheck-clj-kondo-0.0.3/flycheck-clj-kondo-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-clj-kondo" "0.0.3" "Add clj-kondo linter to flycheck" '((flycheck "0.18")) :commit "a558bda44c4cb65b69fa53df233e8941ebd195c5" :authors '(("Michiel Borkent" . "michielborkent@gmail.com")) :maintainer '("Michiel Borkent" . "michielborkent@gmail.com") :url "https://github.com/borkdude/flycheck-clj-kondo")
