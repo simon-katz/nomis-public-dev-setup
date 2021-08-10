1fd6221af76a8b6ade4b0dd30e2183cd888929e3
Simon Katz <sk@nomistech.com>
2021-08-10 18:56:13 +0100

    Upgrade clojure-mode & company; install lsp-mode & lsp-treemacs

---
+++ emacs-installation/.emacs.d/elpa/company-20210809.1354/company-pkg.el
@@ -0,0 +1,12 @@
+(define-package "company" "20210809.1354" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "6b66407b6c7e669bc049deb74c42cd99155daeb4" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainer
+  '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
