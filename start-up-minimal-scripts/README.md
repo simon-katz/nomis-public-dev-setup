63cb8618e5a0a61fef52130c982d951142489334
Simon Katz <sk@nomistech.com>
2021-08-18 20:45:18 +0100

    Add some minimal Emacs startup scripts

---
+++ start-up-minimal-scripts/README.md
@@ -0,0 +1,5 @@
+Run these like this:
+
+    /Applications/Emacs-26-3.app/Contents/MacOS/Emacs -q -l lsp-start-plain.el
+
+    /Applications/Emacs-27-2-2.app/Contents/MacOS/Emacs -q -l lsp-start-plain.el
---
+++ start-up-minimal-scripts/nomis-start-plain.el
@@ -0,0 +1,36 @@
+;;; nomis-start-plain.el --- Plain Emacs quick starter      -*- lexical-binding: t; -*-
+
+;; This file is a helper to start a plain Emacs environment.
+;; To use this, start your Emacs with "emacs -q" and load this file.
+
+;; It forces Emacs to load `.el' files rather than `.elc' files for more
+;; readable backtrace.
+
+(require 'package)
+
+(setq debug-on-error t
+      no-byte-compile t
+      package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/"))
+      package-user-dir (expand-file-name (make-temp-name "nomis-tmp-elpa")
+                                         user-emacs-directory)
+      custom-file (expand-file-name "custom.el" package-user-dir))
+
+(let* ((pkg-list '(;; Add package names here if you want more than a plain Emacs.
+                   )))
+
+  (when pkg-list
+    (package-initialize)
+    (package-refresh-contents))
+
+  (mapc (lambda (pkg)
+          (unless (package-installed-p pkg)
+            (package-install pkg))
+          (require pkg))
+        pkg-list)
+
+  (add-hook 'kill-emacs-hook `(lambda ()
+                                (delete-directory ,package-user-dir t))))
+
+(provide 'nomis-start-plain)
+;;; nomis-start-plain.el ends here
