bbd778d3207852e54305de27c736ffdbc4a7ed9f
Simon Katz <sk@nomistech.com>
2021-08-24 14:07:21 +0100

    Simplify setup of using return in `y-or-n-p`

--- emacs-installation/emacs-init-files/nomis-y-or-n-p.el
+++ emacs-installation/emacs-init-files/nomis-y-or-n-p.el
@@ -13,18 +13,7 @@
 
 ;;;; ___________________________________________________________________________
 
-(defvar nomis/y-or-n-p-query-replace-map-with-ret-for-y
-  (let* ((m (copy-keymap query-replace-map)))
-    (define-key m [return] 'act)
-    m))
-
-(advice-add
- 'y-or-n-p
- :around
- (lambda (orig-func &rest args)
-   (let* ((query-replace-map nomis/y-or-n-p-query-replace-map-with-ret-for-y))
-     (apply orig-func args)))
- '((name . nomis/ret-for-y)))
+(define-key y-or-n-p-map [return] 'act)
 
 ;;;; ___________________________________________________________________________
 
