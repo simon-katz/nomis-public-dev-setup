335c8eda4d3352c18f90d2bf17ffeaf10f5cedf9
Simon Katz <sk@nomistech.com>
2021-09-15 19:40:42 +0100

    Sort `my-packages` and `package-selected-packages`

--- emacs-configuration/_make-emacs-installation.el
+++ emacs-configuration/_make-emacs-installation.el
@@ -12,73 +12,65 @@
 
 (package-refresh-contents)
 
-(defvar my-packages '(elisp-slime-nav
-                      cl-lib
-                      paredit
-                      paxedit
-                      rainbow-delimiters
-                      auto-complete
-                      saveplace
-                      fuzzy
-                      ;; htmlize
-                      pos-tip
-                      magit
-                      ido-completing-read+
-                      smex
-                      ;; idle-highlight-mode -- see "emacs-installation/emacs-init-files/manually-installed/nomis-idle-highlight-mode.el"
-                      ;; scala-mode2
-                      undo-tree
-                      ;; ac-nrepl
-                      clojure-mode
-                      cider
-                      cider-eval-sexp-fu
-                      edn ; needed by clj-refactor, I think
-                      inflections ; needed by clj-refactor, I think
-		      ;; clj-refactor -- you have cloned this repo yourself
-                      ;; cljr-helm -- you have cloned this repo yourself
-                      ;; align-cljlet -- you have cloned this repo yourself
-                      exec-path-from-shell
-                      js2-mode
-                      ;; ac-js2
-                      projectile
-                      company
-                      ;; rcirc-notify
-                      markdown-mode
-                      hydra
-                      key-chord
-                      yasnippet
-                      multiple-cursors
-                      ht
-                      helm
-                      ido-vertical-mode
-                      aggressive-indent
-                      which-key
-                      fill-column-indicator
-                      yaml-mode
-                      yafolding
-                      highlight-indentation
-                      discover
-                      flycheck
-                      flycheck-joker
-                      treepy
-                      cl-format
-                      parseedn
-                      dumb-jump
-                      org-bullets
-                      flycheck-clj-kondo
-                      auto-dim-other-buffers
-                      loccur
-                      logview
-                      vterm
-                      keycast
-                      flycheck-kotlin
-                      kotlin-mode
-                      terraform-mode
-                      lsp-ui
-                      lsp-treemacs
-                      lsp-mode
-                      json-mode
-                      gnu-elpa-keyring-update))
+(defvar my-packages
+  '(aggressive-indent
+    auto-complete
+    auto-dim-other-buffers
+    cider
+    cider-eval-sexp-fu
+    cl-format
+    cl-lib
+    clojure-mode
+    company
+    discover
+    dumb-jump
+    edn ; needed by clj-refactor, I think
+    elisp-slime-nav
+    exec-path-from-shell
+    fill-column-indicator
+    flycheck
+    flycheck-clj-kondo
+    flycheck-joker
+    flycheck-kotlin
+    fuzzy
+    gnu-elpa-keyring-update
+    helm
+    highlight-indentation
+    ht
+    hydra
+    ido-completing-read+
+    ido-vertical-mode
+    inflections ; needed by clj-refactor, I think
+    js2-mode
+    json-mode
+    key-chord
+    keycast
+    kotlin-mode
+    loccur
+    logview
+    lsp-mode
+    lsp-treemacs
+    lsp-ui
+    magit
+    markdown-mode
+    multiple-cursors
+    org-bullets
+    paredit
+    parseedn
+    paxedit
+    pos-tip
+    projectile
+    rainbow-delimiters
+    saveplace
+    smex
+    terraform-mode
+    treepy
+    undo-tree
+    vterm
+    which-key
+    yafolding
+    yaml-mode
+    yasnippet))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
