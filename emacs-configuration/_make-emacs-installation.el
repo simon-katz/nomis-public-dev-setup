7c801c10713da76a5418abe5d00b0b8781b4f5c0
Simon Katz <sk@nomistech.com>
2021-09-15 19:44:43 +0100

    Change some whitespace in a weird way and add a comment

--- emacs-configuration/_make-emacs-installation.el
+++ emacs-configuration/_make-emacs-installation.el
@@ -13,68 +13,71 @@
 (package-refresh-contents)
 
 (defvar my-packages
-  '(aggressive-indent
-    auto-complete
-    auto-dim-other-buffers
-    cider
-    cider-eval-sexp-fu
-    cl-format
-    cl-lib
-    clojure-mode
-    company
-    dash-functional
-    discover
-    dumb-jump
-    edn ; needed by clj-refactor, I think
-    elisp-slime-nav
-    exec-path-from-shell
-    fill-column-indicator
-    flycheck
-    flycheck-clj-kondo
-    flycheck-joker
-    flycheck-kotlin
-    fuzzy
-    gnu-elpa-keyring-update
-    helm
-    highlight-indentation
-    ht
-    hydra
-    ido-completing-read+
-    ido-vertical-mode
-    inflections ; needed by clj-refactor, I think
-    js2-mode
-    json-mode
-    key-chord
-    keycast
-    kotlin-mode
-    loccur
-    logview
-    lsp-mode
-    lsp-treemacs
-    lsp-ui
-    magit
-    markdown-mode
-    multiple-cursors
-    org
-    org-bullets
-    paredit
-    parseedn
-    paxedit
-    pos-tip
-    projectile
-    queue
-    rainbow-delimiters
-    saveplace
-    smex
-    spinner
-    terraform-mode
-    treepy
-    undo-tree
-    vterm
-    which-key
-    yafolding
-    yaml-mode
-    yasnippet))
+  ;; c.f. `package-selected-packages` in "emacs-installation/.emacs".
+  ;; The strange space below is so that we have the same indentation as we
+  ;; have for `package-selected-packages`.
+  ' (aggressive-indent
+     auto-complete
+     auto-dim-other-buffers
+     cider
+     cider-eval-sexp-fu
+     cl-format
+     cl-lib
+     clojure-mode
+     company
+     dash-functional
+     discover
+     dumb-jump
+     edn ; needed by clj-refactor, I think
+     elisp-slime-nav
+     exec-path-from-shell
+     fill-column-indicator
+     flycheck
+     flycheck-clj-kondo
+     flycheck-joker
+     flycheck-kotlin
+     fuzzy
+     gnu-elpa-keyring-update
+     helm
+     highlight-indentation
+     ht
+     hydra
+     ido-completing-read+
+     ido-vertical-mode
+     inflections ; needed by clj-refactor, I think
+     js2-mode
+     json-mode
+     key-chord
+     keycast
+     kotlin-mode
+     loccur
+     logview
+     lsp-mode
+     lsp-treemacs
+     lsp-ui
+     magit
+     markdown-mode
+     multiple-cursors
+     org
+     org-bullets
+     paredit
+     parseedn
+     paxedit
+     pos-tip
+     projectile
+     queue
+     rainbow-delimiters
+     saveplace
+     smex
+     spinner
+     terraform-mode
+     treepy
+     undo-tree
+     vterm
+     which-key
+     yafolding
+     yaml-mode
+     yasnippet))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
