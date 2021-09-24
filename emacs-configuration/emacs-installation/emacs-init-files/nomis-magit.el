498b8653952e1fcfff8fe660963e343e694ed28f
Simon Katz <sk@nomistech.com>
2021-09-24 16:49:53 +0100

    Remove note suggesting incorrectly that Magit might politer now

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-magit.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-magit.el
@@ -8,10 +8,6 @@
 
 (magit-auto-revert-mode 0)
 
-;;;; TODO: I'm not sure, but maybe Magit no longer sets auto-revert-mode
-;;;;       for buffers, and maybe it just reverts them.
-;;;;       Try this out.
-
 ;;;; ___________________________________________________________________________
 
 (provide 'nomis-magit)
