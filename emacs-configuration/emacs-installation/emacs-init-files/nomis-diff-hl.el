8d7291bb0e2eb61f64380334602ff4f505b9d351
Simon Katz <sk@nomistech.com>
2021-09-26 21:01:16 +0100

    diff-hl tailoring and hacking

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-diff-hl.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-diff-hl.el
@@ -16,10 +16,111 @@
 
 (add-hook 'dired-mode-hook 'diff-hl-dired-mode-unless-remote)
 
+(setq diff-hl-show-hunk-inline-popup-hide-hunk t)
+(setq diff-hl-show-hunk-inline-popup-smart-lines nil)
+
 (setq diff-hl-show-hunk-function
-      (case :other
+      (case :default
         (:default 'diff-hl-show-hunk-inline-popup)
         (:other   'diff-hl-show-hunk-posframe)))
+
+(with-eval-after-load 'diff-hl-inline-popup
+
+  (defconst nomis/diff-hl/-scroll-indicator-for-header
+    " ▲ ▲ ▲ ▲")
+
+  (defconst nomis/diff-hl/-scroll-indicator-for-footer
+    " ▼ ▼ ▼ ▼")
+
+  (defconst nomis/diff-hl/-no-scroll-indicator-for-header
+    (make-string (length nomis/diff-hl/-scroll-indicator-for-header)
+                 ?\s))
+
+  (defconst nomis/diff-hl/-no-scroll-indicator-for-footer
+    (make-string (length nomis/diff-hl/-scroll-indicator-for-footer)
+                 ?\s))
+
+  (defconst nomis/diff-hl/-header-footer-colour "blue3")
+
+  (defconst nomis/diff-hl/-header-footer-style/normal
+    (list :underline  t
+          :overline   t
+          :foreground nomis/diff-hl/-header-footer-colour))
+
+  (defconst nomis/diff-hl/-header-style/showing-scroll-arrows
+    (list :overline   t
+          :foreground nomis/diff-hl/-header-footer-colour))
+
+  (defconst nomis/diff-hl/-footer-style/showing-scroll-arrows
+    (list :underline t
+          :foreground nomis/diff-hl/-header-footer-colour))
+
+  (cond
+   ((member (pkg-info-package-version 'diff-hl)
+            '((20210909 207)))
+
+    (defun diff-hl-inline-popup--compute-header (width &optional header)
+      ;; The original `diff-hl-inline-popup--compute-header` is in
+      ;; `diff-hl-inline-popup`.
+      "Compute the header of the popup, with some WIDTH, and some optional HEADER text."
+      ;; :nomis-hack
+      (let* ((show-scroll-arrows?
+              (not (eql diff-hl-inline-popup--current-index 0)))
+             (scroll-indicator (if show-scroll-arrows?
+                                   nomis/diff-hl/-scroll-indicator-for-header
+                                 nomis/diff-hl/-no-scroll-indicator-for-header))
+             (header (or header ""))
+             (new-width (- width (length header) (length scroll-indicator)))
+             (header (if (< new-width 0) "" header))
+             (new-width (- width (length header) (length scroll-indicator)))
+             (line
+              (concat
+               (propertize
+                (concat (diff-hl-inline-popup--separator new-width)
+                        header)
+                'face nomis/diff-hl/-header-footer-style/normal)
+               (propertize
+                scroll-indicator
+                'face (if show-scroll-arrows?
+                          nomis/diff-hl/-header-style/showing-scroll-arrows
+                        nomis/diff-hl/-header-footer-style/normal)))))
+        (concat line "\n") ))
+
+    (defun diff-hl-inline-popup--compute-footer (width &optional footer)
+      ;; The original `diff-hl-inline-popup--compute-footer` is in
+      ;; `diff-hl-inline-popup`.
+      "Compute the header of the popup, with some WIDTH, and some optional FOOTER text."
+      ;; :nomis-hack
+      (let* ((show-scroll-arrows?
+              (not (>= diff-hl-inline-popup--current-index
+                       (- (length diff-hl-inline-popup--current-lines)
+                          diff-hl-inline-popup--height))))
+             (scroll-indicator (if show-scroll-arrows?
+                                   nomis/diff-hl/-scroll-indicator-for-footer
+                                 nomis/diff-hl/-no-scroll-indicator-for-footer))
+             (footer (or footer ""))
+             (new-width (- width (length footer) (length scroll-indicator)))
+             (footer (if (< new-width 0) "" footer))
+             (new-width (- width (length footer) (length scroll-indicator)))
+             (blank-line (if (display-graphic-p)
+                             ""
+                           (concat "\n" (propertize (diff-hl-inline-popup--separator width)
+                                                    'face '(:underline t)))))
+             (line (concat
+                    (propertize
+                     (concat (diff-hl-inline-popup--separator new-width)
+                             footer)
+                     'face nomis/diff-hl/-header-footer-style/normal)
+                    (propertize
+                     scroll-indicator
+                     'face (if show-scroll-arrows?
+                               nomis/diff-hl/-footer-style/showing-scroll-arrows
+                             nomis/diff-hl/-header-footer-style/normal)))))
+        (concat blank-line "\n" line))))
+
+   (t
+    (message-box
+     "You need to fix your hacks for this version of `diff-hl-inline-popup`."))))
 
 ;;;; ___________________________________________________________________________
 
