d879ae0a1521462a671c2503e1ea0d416b037d85
Simon Katz <sk@nomistech.com>
2021-09-29 18:27:41 +0100

    Improve reverting buffers

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-revert.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-revert.el
@@ -3,15 +3,33 @@
 (require 'dash)
 (require 's)
 (require 'nomis-msg)
+(require 'ido)
 
 ;;;; ___________________________________________________________________________
 ;;;; ---- Various utilities ----
 
+(defun nomis/prompt-using-value-string-pairs (prompt
+                                              value-string-pairs
+                                              &optional
+                                              history-list-name)
+  (let* ((s (ido-completing-read prompt
+                                 (-map #'second value-string-pairs)
+                                 nil
+                                 t
+                                 nil
+                                 history-list-name)))
+    (first (-find (lambda (pair) (equal (second pair) s))
+                  value-string-pairs))))
+
 (defun nomis/vc-buffer-in-current-repo? (b)
   (case 2
     (1 (magit-auto-revert-repository-buffer-p b))
-    (2 (s-starts-with? (nomis/dirtree/vc-root-dir)
-                       (buffer-file-name b)))))
+    (2 (let* ((vc-root-dir (nomis/dirtree/vc-root-dir)))
+         (when (null vc-root-dir)
+           (beep)
+           (error "Not in a repository"))
+         (s-starts-with? vc-root-dir
+                         (buffer-file-name b))))))
 
 (defun nomis/-vc-make/buffer-in-current-repo?-fun ()
   (let* ((this-buffer (current-buffer)))
@@ -19,23 +37,12 @@
       (with-current-buffer this-buffer
         (nomis/vc-buffer-in-current-repo? b)))))
 
-(defun nomis/-vc-buffer-unmodified? (in-current-repo?-fun b)
-  (and (buffer-file-name b)
-       (not (buffer-modified-p b))
-       (funcall in-current-repo?-fun b)))
-
-(defun nomis/-vc-buffer-unmodified-and-out-of-date? (in-current-repo?-fun b)
-  (and (nomis/-vc-buffer-unmodified? in-current-repo?-fun b)
-       (not (verify-visited-file-modtime b))))
-
 (defun nomis/find-buffers (&rest buffer-predicates)
-  (let* ((buffers '()))
-    (dolist (buf (buffer-list))
-      (with-current-buffer buf
-        (when (-every? (lambda (pred) (funcall pred buf))
-                       buffer-predicates)
-          (push buf buffers))))
-    buffers))
+  (-filter (lambda (b)
+             (with-current-buffer b
+               (-every? (lambda (pred) (funcall pred b))
+                        buffer-predicates)))
+           (buffer-list)))
 
 (defun nomis/with-cleanup-on-non-local-exit/fun (f cleanup-f)
   (let* ((non-local-exit? t))
@@ -56,87 +63,132 @@
     (nomis/message-no-disp "==== Quitting")))
 
 (defun nomis/buffers->string-of-names (buffers)
-  (s-join " "
-          (-map #'buffer-file-name buffers)))
+  (cl-flet ((buffer-desc
+             (b)
+             (format "%s%s %s"
+                     (if (verify-visited-file-modtime b) " " "!")
+                     (if (not (buffer-modified-p b))     " " "*")
+                     (buffer-file-name b))))
+    (->> buffers
+         (-sort (lambda (x y)
+                  (s-less? (buffer-file-name x)
+                           (buffer-file-name y))))
+         (-map #'buffer-desc)
+         (s-join "\n"))))
 
 ;;;; ___________________________________________________________________________
 ;;;; ---- Reverting buffers ----
 
-(defun nomis/revert-buffer/simple ()
-  (revert-buffer t t t))
-
-(defun nomis/revert-buffer-reporting-failures (&optional b)
-  (let* ((b (or b (current-buffer))))
-    (with-current-buffer b
-      (condition-case e
-          (nomis/revert-buffer/simple)
-        (error
-         (nomis/message-no-disp "==== Failed to revert %s" b))))))
-
-(defun nomis/revert-all-buffers (buffer-predicate inhibit-message?)
-  "Revert all buffers that satisfy `buffer-predicate`.
-For each buffer, the predicate is run with the buffer as the
-current buffer. Unless `inhibit-message?` is non-nil, emit
-a message when finished. The message includes the names of any
-buffers that could not be reverted."
-  ;; Copy-and-hack of http://www.emacswiki.org/emacs/RevertBuffer
-  (let ((failures '()))
-    (dolist (buf (buffer-list))
-      (with-current-buffer buf
-        (when (and (buffer-file-name) (funcall buffer-predicate buf))
+(defun nomis/-revert-buffers* (buffers-to-revert preserve-modes?)
+  (let* ((reverted-buffers '())
+         (failures '()))
+    (dolist (b buffers-to-revert)
+      (with-current-buffer b
+        (let* ((file-name (buffer-file-name b)))
           (condition-case e
-              (nomis/revert-buffer/simple)
+              (progn (revert-buffer t t preserve-modes?)
+                     (nomis/message-no-disp "Reverted %s" file-name)
+                     (push b reverted-buffers))
             (error
-             (push buf failures)
-             (nomis/message-no-disp "%s" e)
-             (beep))))))
-    (let* ((inhibit-message inhibit-message?))
-      (message "Refreshed open files. %s"
-               (if failures
-                   (s-join " " (cons "Failures: "
-                                     (-map #'buffer-file-name failures)))
-                 "")))))
+             (push b failures)
+             (nomis/message-no-disp "**** Failed to revert %s because: %s"
+                                    file-name
+                                    e))))))
+    (message
+     "%s"
+     (let* ((lines
+             (-concat
+              (list (format "Reverted %s buffer(s)"
+                            (length reverted-buffers)))
+              (when failures
+                (list "--------------------------------------------------------------------------------"
+                      (format "Failed to revert the following %s buffer(s):"
+                              (length failures))
+                      (nomis/buffers->string-of-names failures)
+                      (format "Failed to revert the above %s buffer(s):"
+                              (length failures))))
+              (list "See *Messages* buffer for details"))))
+       (s-join "\n" lines)))
+    (when failures (beep))))
 
-(defun nomis/revert-all-unmodified-buffers (inhibit-message?)
-  "Refreshes all open unmodified buffers from their files."
-  (interactive "P")
-  (nomis/revert-all-buffers (lambda (b) (not (buffer-modified-p b)))
-                            inhibit-message?))
+(defun nomis/revert/-prompt-for-mode ()
+  (nomis/prompt-using-value-string-pairs
+   "Which buffers do you want to revert? "
+   '((:unmodified-buffers-only         "Unmodified buffers only")
+     (:out-of-sync-unmodified-only     "Out-of-sync unmodified buffers only")
+     (:modified-buffers-only           "Modified buffers only")
+     (:modified-and-unmodified-buffers "Modified and unmodified buffers")
+     (:no-revert-when-unmodified-exist "Don't revert when there are unmodified buffers"))
+   'nomis/revert/-prompt-for-mode))
 
-(defun nomis/revert-all-modified-buffers (inhibit-message?)
-  "Refreshes all open modified buffers from their files."
-  ;; Copied from http://www.emacswiki.org/emacs/RevertBuffer, and renamed.
-  (interactive "P")
-  (when (nomis/y-or-n-p-reporting-non-local-exit
-         "Really revert modified buffers? You will lose stuff.")
-    (nomis/revert-all-buffers 'buffer-modified-p
-                              inhibit-message?)))
+(defun nomis/revert/-prompt-for-restrict-to-current-repo ()
+  (nomis/prompt-using-value-string-pairs
+   "Restrict to current repository? "
+   '((t   "Yes, restrict to buffers in the current repository")
+     (nil "No, don't restrict"))
+   'nomis/revert/-prompt-for-restrict-to-current-repo))
 
-;;;; ___________________________________________________________________________
+(defun nomis/revert/-prompt-for-preserve-modes ()
+  (nomis/prompt-using-value-string-pairs
+   "Preserve buffer modes? "
+   '((t   "Yes, leave modes as they are")
+     (nil "No, revert modes"))
+   'nomis/revert/-prompt-for-preserve-modes))
 
-(defun nomis/revert-out-of-sync-buffers-in-repo
-    (&optional force?)
-  (interactive "P")
-  (let* ((in-current-repo?-fun (nomis/-vc-make/buffer-in-current-repo?-fun))
-         (revert-buffer?-fun (lambda (b)
-                               (and (buffer-file-name b)
-                                    (not (buffer-modified-p b))
-                                    (funcall in-current-repo?-fun b)
-                                    (not (verify-visited-file-modtime b)))))
-         (buffers-to-maybe-revert (nomis/find-buffers revert-buffer?-fun)))
-    (if (null buffers-to-maybe-revert)
-        (message "No buffers are out-of-sync")
-      (let* ((revert-buffers?
-              (or force?
-                  (nomis/y-or-n-p-reporting-non-local-exit
-                   (format
-                    "Do you want to revert the following %s buffer(s) that are out-of-sync with their files? %s"
-                    (length buffers-to-maybe-revert)
-                    buffers-to-maybe-revert)))))
-        (when revert-buffers?
-          (dolist (b buffers-to-maybe-revert)
-            (nomis/message-no-disp "==== ==== Reverting %s" b)
-            (nomis/revert-buffer-reporting-failures b)))))))
+(defun nomis/revert-buffers (no-confirm?
+                             mode
+                             only-current-repo?
+                             preserve-modes?)
+  "Revert buffers."
+  (interactive (list current-prefix-arg
+                     (nomis/revert/-prompt-for-mode)
+                     (nomis/revert/-prompt-for-restrict-to-current-repo)
+                     (nomis/revert/-prompt-for-preserve-modes)))
+  (cl-multiple-value-bind (revert-modified-buffers?
+                           revert-unmodified-buffers?
+                           out-of-sync-buffers-only?
+                           bail-out-when-modified-buffers?)
+      (ecase mode
+        (:unmodified-buffers-only         '(nil t   nil nil))
+        (:out-of-sync-unmodified-only     '(nil t   t   nil))
+        (:modified-buffers-only           '(t   nil nil nil))
+        (:modified-and-unmodified-buffers '(t   t   nil nil))
+        (:no-revert-when-unmodified-exist '(nil t   nil t)))
+    (let* ((buffer-predicates
+            (-concat (when only-current-repo?
+                       (list (nomis/-vc-make/buffer-in-current-repo?-fun)))
+                     (when out-of-sync-buffers-only?
+                       (list (-compose #'not #'verify-visited-file-modtime)))))
+           (buffers-to-consider (apply #'nomis/find-buffers
+                                       #'buffer-file-name
+                                       buffer-predicates))
+           (modified-buffers (-filter #'buffer-modified-p
+                                      buffers-to-consider)))
+      (if (and bail-out-when-modified-buffers?
+               modified-buffers)
+          (progn
+            (beep)
+            (message "The following %s buffers are unsaved:\n%s\nThe above %s buffer(s) are unsaved.\nSave them or revert them first."
+                     (length modified-buffers)
+                     (nomis/buffers->string-of-names modified-buffers)
+                     (length modified-buffers)))
+        (let* ((unmodified-buffers (-filter (-compose #'not #'buffer-modified-p)
+                                            buffers-to-consider))
+               (buffers-to-revert (-concat (when revert-modified-buffers?
+                                             modified-buffers)
+                                           (when revert-unmodified-buffers?
+                                             unmodified-buffers))))
+          (if (null buffers-to-revert)
+              (progn (beep)
+                     (message "There are no buffers to revert"))
+            (when (or no-confirm?
+                      (nomis/y-or-n-p-reporting-non-local-exit
+                       (format
+                        "%s\nAre you sure you want to revert the above %s buffer(s)?"
+                        (nomis/buffers->string-of-names buffers-to-revert)
+                        (length buffers-to-revert))))
+              (nomis/-revert-buffers* buffers-to-revert
+                                      preserve-modes?))))))))
 
 ;;;; ___________________________________________________________________________
 
