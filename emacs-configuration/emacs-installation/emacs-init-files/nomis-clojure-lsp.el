75fa84fb06c09943e73fa9e1f96860dee2825414
Simon Katz <sk@nomistech.com>
2021-10-04 16:10:42 +0100

    Re-introduce `nomis/clojure-lsp-and-cider/find-definition`
    Also improve it to use LSP if the thing at point is a symbol.

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-clojure-lsp.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-clojure-lsp.el
@@ -25,38 +25,46 @@
 ;;;; ___________________________________________________________________________
 ;;;; ---- nomis/clojure-lsp-and-cider/find-definition ----
 
-;; Not needed now that `cider-xref-fn-depth` has been introduced.
+;; We had commented this out because `cider-xref-fn-depth` has been introduced
+;; (meaning CIDER is used in preference to LSP), but we've re-introduced it
+;; because LSP lets us find clojure.spec definitions for symbols.
 
-;; (defun nomis/clojure-lsp-and-cider/find-definition ()
-;;   "Try to find definition of thing at point using CIDER; if no luck try using LSP."
-;;   (interactive)
-;;   (cl-flet ((buffer-and-point () (list (current-buffer) (point))))
-;;     (let* ((old-bap (buffer-and-point)))
-;;       (when (cider-repls)
-;;         (cider-find-var))
-;;       (let* ((new-bap (buffer-and-point)))
-;;         (when (equal old-bap new-bap)
-;;           (beep)
-;;           (message "Couldn't find definition using CIDER -- trying LSP")
-;;           (lsp-find-definition))))))
+(defun nomis/clojure-lsp-and-cider/find-definition ()
+  "Try to find definition of thing at point.
+ If the thing at point is a keyword, use LSP. If the thing at
+ point is a not a keyword, try using CIDER; if no luck with
+ CIDER, use LSP."
+  (interactive)
+  (if (ignore-errors (s-starts-with? ":" (symbol-name (symbol-at-point))))
+      (lsp-find-definition)
+    (cl-flet ((buffer-and-point () (list (current-buffer) (point))))
+      (let* ((old-bap (buffer-and-point)))
+        (when (cider-repls)
+          (cider-find-var))
+        (let* ((new-bap (buffer-and-point)))
+          (when (equal old-bap new-bap)
+            (beep)
+            (message "Couldn't find definition using CIDER -- trying LSP")
+            (lsp-find-definition)))))))
 
-;; (with-eval-after-load 'cider
-;;   (with-eval-after-load 'lsp-mode
-;;     (cond
-;;      ((member (list (pkg-info-package-version 'cider)
-;;                     (pkg-info-package-version 'lsp-mode) ; Hmmm, I guess you really want to depend on the version of `clojure-lsp` executable installed on the system.
-;;                     )
-;;               '(((20210909 1011) (20210821 1359))))
+(with-eval-after-load 'cider
+  (with-eval-after-load 'lsp-mode
+    (cond
+     ((member (list (pkg-info-package-version 'cider)
+                    (pkg-info-package-version 'lsp-mode) ; Hmmm, I guess you really want to depend on the version of `clojure-lsp` executable installed on the system.
+                    )
+              '(((20210909 1011) (20210821 1359))
+                ((20210929 1032) (20210821 1359))))
 
-;;       (dolist (m (list clojure-mode-map
-;;                        cider-mode-map
-;;                        clojurec-mode-map
-;;                        clojurescript-mode-map))
-;;         (define-key m (kbd "M-.") 'nomis/clojure-lsp-and-cider/find-definition)))
+      (dolist (m (list clojure-mode-map
+                       cider-mode-map
+                       clojurec-mode-map
+                       clojurescript-mode-map))
+        (define-key m (kbd "M-.") 'nomis/clojure-lsp-and-cider/find-definition)))
 
-;;      (t
-;;       (message-box
-;;        "You need to fix `nomis/clojure-lsp-and-cider/find-definition` for this version of CIDER and LSP.")))))
+     (t
+      (message-box
+       "You need to fix `nomis/clojure-lsp-and-cider/find-definition` for this version of CIDER and LSP.")))))
 
 ;;;; ___________________________________________________________________________
 
