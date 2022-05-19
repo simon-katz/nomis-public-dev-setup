cbc9186d5016d13d90e6356906e1f57253903e8d
Simon Katz <sk@nomistech.com>
2022-05-20 00:03:02 +0100

    WIP: Add nomis-spaces-up-grid.clj

--- nomis-bin/nomis-spaces-down-grid.clj
+++ nomis-bin/nomis-spaces-down-grid.clj
@@ -1,22 +1,22 @@
 #!/usr/bin/env /usr/local/bin/bb
 
-(def space->next-space
-  {1   5
-   2   6
-   3   7
-   4   8
-   5   9
-   6  10
-   7  11
-   8  12
-   9  13
-   10 14
-   11 15
-   12 16
-   13  1
-   14  2
-   15  3
-   16  4})
+(def space->next-space-details
+  {1  [ 5 false]
+   2  [ 6 false]
+   3  [ 7 false]
+   4  [ 8 false]
+   5  [ 9 false]
+   6  [10 false]
+   7  [11 false]
+   8  [12 false]
+   9  [13 false]
+   10 [14 false]
+   11 [15 false]
+   12 [16 false]
+   13 [ 1 true]
+   14 [ 2 true]
+   15 [ 3 true]
+   16 [ 4 true]})
 
 (def ^:private space->key-spec
   ;; See https://eastmanreference.com/complete-list-of-applescript-key-codes
@@ -68,12 +68,20 @@ end tell")
                          (re-find #"macos-desktop-backgrounds:([0-9]*)")
                          second
                          parse-long)
-      new-space (space->next-space current-space)]
-  (if new-space
-    (go-to-space new-space)
-    ;; TODO: Flash screen if `(= current-space new-space)`
-    nil)
-  #_
+      [new-space wrapped?] (space->next-space-details current-space)]
+  (go-to-space new-space)
+  (when wrapped?
+    (case 5
+      0 (osa "tell application \"System Events\" to repeat 2 times
+key code 18 using {command down, control down, option down}
+delay 0
+end repeat")
+      1 (osa "display alert \"wrapped\" buttons {\"Wrapped\"} giving up after 1")
+      2 (osa "beep")
+      ;; TODO: Not working.
+      3 (shell/sh "/Users/simonkatz/development-100/repositories/nomis/dev-setup/nomis-public-dev-setup/nomis-bin/bells")
+      4 (osa "display alert \"wrapped\" buttons {\"•\"} giving up after 1")
+      5 (osa "display dialog \"wrapped\" buttons {\"•\"} giving up after 1")))
   (shell/sh "touch"
             (str "/Users/simonkatz/development-100/repositories/zzzz-nomis-spaces-up-grid-ran-"
                  new-space))
--- nomis-bin/nomis-spaces-up-grid.clj
+++ nomis-bin/nomis-spaces-up-grid.clj
@@ -1,22 +1,22 @@
 #!/usr/bin/env /usr/local/bin/bb
 
-(def space->next-space
-  {1   13
-   2   14
-   3   15
-   4   16
-   5   1
-   6   2
-   7   3
-   8   4
-   9   5
-   10  6
-   11  7
-   12  8
-   13  9
-   14 10
-   15 11
-   16 12})
+(def space->next-space-details
+  {1  [13 true]
+   2  [14 true]
+   3  [15 true]
+   4  [16 true]
+   5  [ 1 false]
+   6  [ 2 false]
+   7  [ 3 false]
+   8  [ 4 false]
+   9  [ 5 false]
+   10 [ 6 false]
+   11 [ 7 false]
+   12 [ 8 false]
+   13 [ 9 false]
+   14 [10 false]
+   15 [11 false]
+   16 [12 false]})
 
 (def ^:private space->key-spec
   ;; See https://eastmanreference.com/complete-list-of-applescript-key-codes
@@ -68,12 +68,20 @@ end tell")
                          (re-find #"macos-desktop-backgrounds:([0-9]*)")
                          second
                          parse-long)
-      new-space (space->next-space current-space)]
-  (if new-space
-    (go-to-space new-space)
-    ;; TODO: Flash screen if `(= current-space new-space)`
-    nil)
-  #_
+      [new-space wrapped?] (space->next-space-details current-space)]
+  (go-to-space new-space)
+  (when wrapped?
+    (case 5
+      0 (osa "tell application \"System Events\" to repeat 2 times
+key code 18 using {command down, control down, option down}
+delay 0
+end repeat")
+      1 (osa "display alert \"wrapped\" buttons {\"Wrapped\"} giving up after 1")
+      2 (osa "beep")
+      ;; TODO: Not working.
+      3 (shell/sh "/Users/simonkatz/development-100/repositories/nomis/dev-setup/nomis-public-dev-setup/nomis-bin/bells")
+      4 (osa "display alert \"wrapped\" buttons {\"•\"} giving up after 1")
+      5 (osa "display dialog \"wrapped\" buttons {\"•\"} giving up after 1")))
   (shell/sh "touch"
             (str "/Users/simonkatz/development-100/repositories/zzzz-nomis-spaces-up-grid-ran-"
                  new-space))
