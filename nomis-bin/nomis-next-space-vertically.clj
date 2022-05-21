8a0a9eda59d504a19fcf02a2dfc631d4a30b4049
Simon Katz <sk@nomistech.com>
2022-05-21 09:38:37 +0100

    Add first draft of documentation

--- nomis-bin/nomis-next-space-vertically.clj
+++ nomis-bin/nomis-next-space-vertically.clj
@@ -1,5 +1,146 @@
 #!/usr/bin/env /usr/local/bin/bb
 
+;;;; Introduction
+;;;; ============
+
+;;;; - TotalSpaces (https://totalspaces.binaryage.com/) is, or was, a great tool
+;;;;   for navigating macOS Spaces (virtual desktops). It is not available for
+;;;;   recent versions of macOS or recent Mac hardware.
+
+;;;; - I hope a new version of TotalSpaces becomes available at some point.
+;;;;   As of today (2022-05-21), that seems to be a possibility -- see
+;;;;   https://discuss.binaryage.com/t/can-we-help-test-total-spaces-3-if-we-have-apple-silicon/8199/134
+
+;;;; - I've hacked a replacement for parts of TotalSpaces's functionality.
+;;;;   And it is very much a hack -- cobbling a bunch of things together.
+
+;;;; - TODO: Explain the Spaces grid.
+
+;;;; - This Script provides the ability to move up and down a grid of Spaces.
+;;;;   The grid exists in your head, not in macOS, so the visual feedback is not
+;;;;   ideal -- it indicates left and right movement when you've moved up
+;;;;   or down.
+
+;;;; - TODO: Explain what you've done in BetterTouchTool.
+;;;;         - Not just calling this script.
+
+
+;;;; Overview
+;;;; ========
+
+;;;; •••
+
+
+;;;; Installation
+;;;; ============
+
+;;;; TODO: Babashka
+
+
+;;;; Set Up
+;;;; ======
+
+;;;; - You need a tool to map keystrokes to actions. I use BetterTouchTool.
+;;;;   Below we call this the keystroke-to-action mapper.
+
+;;;; - Using your keystroke-to-action mapper, set up keystrokes to invoke the
+;;;;   following commands:
+;;;;   - <path-to-this-script> down
+;;;;   - <path-to-this-script> up
+
+;;;; - Set up keyboard shortcuts to switch between Spaces:
+;;;;     System Preferences / Keyboard / Shortcuts / Mission Control
+;;;;     - Switch to Desktop  1: Control-1
+;;;;     - Switch to Desktop  2: Control-2
+;;;;       etc
+;;;;     - Switch to Desktop 10: Control-0
+;;;;     - Switch to Desktop 11: Control-Option-1
+;;;;     - Switch to Desktop 12: Control-Option-2
+;;;;     - etc
+;;;;   This script invokes those keystrokes to switch between Spaces.
+
+;;;; - When wrapping across the top or bottom of the Spaces grid, this script
+;;;;   tries to flash the screen. If you want that to happen, turn on the
+;;;;   following: System Preferences / Accessibility / Audio / Flash the screen
+;;;;   when an alert sound occurs
+
+;;;; - Current Space Number
+;;;;   - This script needs a way to find the current Space number.
+;;;;   - It seems that recent versions of AppleScript don't provide this.
+;;;;   - Maybe there's a better way, but for now at least we set up each Space
+;;;;     to have a unique background image, and we use that to identify the
+;;;;     Space. /NB/ This means that if you change the order of Spaces you will
+;;;;     break this script's idea of the grid.
+;;;;   - We got the idea from https://stackoverflow.com/a/34691386/2148181
+;;;;   - TODO: Move your macos-desktop-backgrounds to this repo (this dir) so
+;;;;           that other people can use them.
+;;;;   - For each Space:
+;;;;     - Create a desktop background picture file. The filename must include
+;;;;       the Space number.
+;;;;     - Set up the desktop background picture in
+;;;;       System Preferences / Desktop & Screen Saver.
+;;;;   - I generated the files using
+;;;;     https://seotoolscentre.com/text-to-image-generator
+;;;;   - I use the following filenames:
+;;;;        1.png
+;;;;        2.png
+;;;;        3.png
+;;;;        4.png
+;;;;        5.png
+;;;;        6.png
+;;;;        7.png
+;;;;        8.png
+;;;;        9.png
+;;;;        10.png
+;;;;        11.png
+;;;;        12.png
+;;;;        13.png
+;;;;        14.png
+;;;;        15.png
+;;;;        16.png
+
+
+;;;; Notes on Use
+;;;; ============
+
+;;;; - After pressing the keys to invoke your keystroke-to-action mapper, you
+;;;;   must release them so that when this script does Control-1 etc to
+;;;;   do the work, no other keys are being pressed.
+;;;;   TODO: Can you somehow "release" the keys in software-land?
+;;;;         I think so. After all, you can press them.
+;;;;         See `go-to-space-applescript-format-string`, for example.
+
+;;;; - Don't change the order of Spaces. Because of the way we get the current
+;;;;   Space number, that will break this script's idea of the grid.
+;;;;   - TODO: Is there a better way to find the current Space number?
+;;;;   - TODO: Maybe, if all Space movement were to come through a single
+;;;;           script, you could remember the current Space. But probably that's
+;;;;           a bad idea because you won't know the initial state and you
+;;;;           probably won't always know when the Space changes.
+
+
+;;;; Other Useful Tools
+;;;; ==================
+
+;;;; - SpaceMan (https://github.com/Jaysce/Spaceman) uses the macOS menu bar to
+;;;;   show which Space is current.
+
+
+;;;; Things I Would Like to Do
+;;;; =========================
+
+;;;; - TODO: Move windows between spaces.
+
+;;;; - TODO: Do horizontal movement so that you can wrap to the same row.
+
+;;;; - TODO: Is it possible to briefly flash an image? If so, that could be used
+;;;;         to provide feedback such as flashing the current Space number or
+;;;;         other graphic after moving Space.
+
+;;;; - TODO: An overview grid. Is there somthing that does this? I don't
+;;;;         think so.
+
+
 ;;;; ___________________________________________________________________________
 ;;;; Stuff that other people might want to change.
 
