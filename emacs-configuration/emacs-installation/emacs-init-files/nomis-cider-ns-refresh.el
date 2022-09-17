b4da613edc85c664b692e2b9df05b1c8ee8edc4c
Simon Katz <sk@nomistech.com>
2022-09-17 13:32:45 +0100

    Apply `nomis/cider-ns-refresh/multiple-lines` advice

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-cider-ns-refresh.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-cider-ns-refresh.el
@@ -290,7 +290,8 @@
 (cond
  ((member (pkg-info-version-info 'cider)
           '("1.2.0snapshot (package: 20211105.708)"
-            "1.3.0 (package: 20220405.1216)"))
+            "1.3.0 (package: 20220405.1216)"
+            "1.5.0 (package: 20220830.500)"))
 
   (defvar *nomis/cider-ns-refresh/-in-handle-response?* nil)
 
