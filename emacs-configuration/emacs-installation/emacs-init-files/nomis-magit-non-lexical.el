d5a809d9966e3debe155f71043a804ede7140b7c
Simon Katz <sk@nomistech.com>
2023-01-14 11:29:38 +0000

    Add "-" prefix to some `nomis/magit-xxxx` things

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-magit-non-lexical.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-magit-non-lexical.el
@@ -3,7 +3,7 @@
 ;;;; ___________________________________________________________________________
 ;;;; ---- Status sections ----
 
-(defun nomis/magit-insert-pushed-commits ()
+(defun -nomis/magit-insert-pushed-commits ()
   "Insert section showing pushed commits.
 Show the last `magit-log-section-commit-count' commits."
   (when (magit-git-success "rev-parse" "@{upstream}")
@@ -22,16 +22,16 @@ Show the last `magit-log-section-commit-count' commits."
                                 (--remove (string-prefix-p "-n" it)
                                           magit-buffer-log-args)))))))
 
-(defun nomis/-magic-init-sections ()
+(defun -nomis/-magic-init-sections ()
   ;; Change what's in Magit status buffers:
   ;; - By default, if there are unpushed commits Magit status doesn't show
   ;;   pushed commits.
   ;;   That's not good.
   ;;   Fix this by replacing `magit-insert-unpushed-to-upstream-or-recent`, with
   ;;   `magit-insert-unpushed-to-upstream` and
-  ;;   `nomis/magit-insert-pushed-commits`.
+  ;;   `-nomis/magit-insert-pushed-commits`.
   (dolist (f '(magit-insert-unpushed-to-upstream
-               nomis/magit-insert-pushed-commits))
+               -nomis/magit-insert-pushed-commits))
     (magit-add-section-hook 'magit-status-sections-hook
                             f
                             'magit-insert-unpushed-to-upstream-or-recent
@@ -46,31 +46,31 @@ Show the last `magit-log-section-commit-count' commits."
 
 ;;;; Make all sections initially expanded.
 
-(defvar nomis/-magit-section-visibility/seens '())
-(make-variable-buffer-local 'nomis/-magit-section-visibility/seens)
-(put 'nomis/-magit-section-visibility/seens 'permanent-local t)
+(defvar -nomis/-magit-section-visibility/seens '())
+(make-variable-buffer-local '-nomis/-magit-section-visibility/seens)
+(put '-nomis/-magit-section-visibility/seens 'permanent-local t)
 
-(defvar nomis/-magit-sections-to-always-expand
+(defvar -nomis/-magit-sections-to-always-expand
   '((unpulled status) ; because the visibility of this gets queried when it has no commits, and Magit decides that we don't want to expand it
     ))
 
-(defun nomis/-magit-section-visibility (section)
+(defun -nomis/-magit-section-visibility (section)
   (let* ((item (magit-section-lineage section)))
-    (if (member item nomis/-magit-sections-to-always-expand)
+    (if (member item -nomis/-magit-sections-to-always-expand)
         'show
-      (let* ((seen? (member item nomis/-magit-section-visibility/seens)))
+      (let* ((seen? (member item -nomis/-magit-section-visibility/seens)))
         (if seen?
             nil
-          (progn (push item nomis/-magit-section-visibility/seens)
+          (progn (push item -nomis/-magit-section-visibility/seens)
                  'show))))))
 
-(defun nomis/-magit-init-section-visibility ()
+(defun -nomis/-magit-init-section-visibility ()
   (add-hook 'magit-section-set-visibility-hook
-            'nomis/-magit-section-visibility))
+            '-nomis/-magit-section-visibility))
 
 ;;;; ___________________________________________________________________________
 
-(defun nomis/init-magit-mode ()
+(defun -nomis/init-magit-mode ()
   (company-mode 0)
   (setq magit-log-margin
         ;; (INIT STYLE WIDTH AUTHOR AUTHOR-WIDTH)
@@ -86,16 +86,16 @@ Show the last `magit-log-section-commit-count' commits."
   ;; (setq git-commit-summary-max-length 999)
   (setq magit-display-buffer-function
         'magit-display-buffer-same-window-except-diff-v1)
-  (nomis/-magic-init-sections)
-  (nomis/-magit-init-section-visibility))
+  (-nomis/-magic-init-sections)
+  (-nomis/-magit-init-section-visibility))
 
-(add-hook 'magit-mode-hook 'nomis/init-magit-mode)
+(add-hook 'magit-mode-hook '-nomis/init-magit-mode)
 
-(defun nomis/init-magit-status-mode ()
+(defun -nomis/init-magit-status-mode ()
   ;; (visual-line-mode 1) ; -- No -- the status buffer gets confused when you expand and collapse sections
   )
 
-(add-hook 'magit-status-mode-hook 'nomis/init-magit-status-mode)
+(add-hook 'magit-status-mode-hook '-nomis/init-magit-status-mode)
 
 (defun nomis/magit/status (arg)
   (interactive "P")
