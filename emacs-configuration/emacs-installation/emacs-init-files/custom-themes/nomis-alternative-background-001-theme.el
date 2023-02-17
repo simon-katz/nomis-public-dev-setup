d675b32526da072e108adc30ff15d5344c4df3a7
Simon Katz <sk@nomistech.com>
2023-02-17 10:53:38 +0000

    Fix `org-hide` face in alternative background faces

--- emacs-configuration/emacs-installation/emacs-init-files/custom-themes/nomis-alternative-background-001-theme.el
+++ emacs-configuration/emacs-installation/emacs-init-files/custom-themes/nomis-alternative-background-001-theme.el
@@ -1,12 +1,22 @@
 (deftheme nomis-alternative-background-001
   "Created 2023-01-03.")
 
+(defconst nomis-alternative-background-001/light-default-background
+  "BlanchedAlmond")
+
+(defconst nomis-alternative-background-001/dark-default-background
+  "#654020")
+
 (custom-theme-set-faces
  'nomis-alternative-background-001
 
  `(default
-    ((((background light)) ,(list :background "BlanchedAlmond"))
-     (((background dark))  ,(list :background "#654020"))))
+    ((((background light))
+      ,(list :background
+             nomis-alternative-background-001/light-default-background))
+     (((background dark))
+      ,(list :background
+             nomis-alternative-background-001/dark-default-background))))
 
  `(auto-dim-other-buffers-face
    ((((background light)) ,(list :background "NavajoWhite1"))
@@ -16,6 +26,14 @@
    ((((background light)) ,(list :foreground "NavajoWhite1"
                                  :background "NavajoWhite1"))
     (((background dark))  ,(list :foreground "#805737"
-                                 :background "#805737")))))
+                                 :background "#805737"))))
+
+ `(org-hide
+   ((((background light))
+     ,(list :foreground
+            nomis-alternative-background-001/light-default-background))
+    (((background dark))
+     ,(list :foreground
+            nomis-alternative-background-001/dark-default-background)))))
 
 (provide-theme 'nomis-alternative-background-001)
--- emacs-configuration/emacs-installation/emacs-init-files/custom-themes/nomis-alternative-background-002-theme.el
+++ emacs-configuration/emacs-installation/emacs-init-files/custom-themes/nomis-alternative-background-002-theme.el
@@ -1,12 +1,22 @@
 (deftheme nomis-alternative-background-002
   "Created 2023-01-03.")
 
+(defconst nomis-alternative-background-002/light-default-background
+  "MistyRose")
+
+(defconst nomis-alternative-background-002/dark-default-background
+  "#603020")
+
 (custom-theme-set-faces
  'nomis-alternative-background-002
 
  `(default
-    ((((background light)) ,(list :background "MistyRose"))
-     (((background dark))  ,(list :background "#603020"))))
+    ((((background light))
+      ,(list :background
+             nomis-alternative-background-002/light-default-background))
+     (((background dark))
+      ,(list :background
+             nomis-alternative-background-002/dark-default-background))))
 
  `(auto-dim-other-buffers-face
    ((((background light)) ,(list :background "MistyRose2"))
@@ -16,6 +26,14 @@
    ((((background light)) ,(list :foreground "MistyRose2"
                                  :background "MistyRose2"))
     (((background dark))  ,(list :foreground "#805040"
-                                 :background "#805040")))))
+                                 :background "#805040"))))
+
+ `(org-hide
+   ((((background light))
+     ,(list :foreground
+            nomis-alternative-background-002/light-default-background))
+    (((background dark))
+     ,(list :foreground
+            nomis-alternative-background-002/dark-default-background)))))
 
 (provide-theme 'nomis-alternative-background-002)
