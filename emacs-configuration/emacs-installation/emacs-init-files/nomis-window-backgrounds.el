6cecf5bf2da683354e3ef41532845447ff5cb288
Simon Katz <sk@nomistech.com>
2023-02-26 19:57:07 +0000

    Add `nomis/auto-dim/with-turn-off-and-restore`

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-window-backgrounds.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-window-backgrounds.el
@@ -12,4 +12,17 @@
 
 ;;;; ___________________________________________________________________________
 
+(defun nomis/auto-dim/with-turn-off-and-restore* (f)
+  (let* ((auto-dimming? (bound-and-true-p auto-dim-other-buffers-mode)))
+    (when auto-dimming? (auto-dim-other-buffers-mode 0))
+    (unwind-protect
+        (funcall f)
+      (when auto-dimming? (auto-dim-other-buffers-mode 1)))))
+
+(cl-defmacro nomis/auto-dim/with-turn-off-and-restore (&body body)
+  (declare (indent 0))
+  `(nomis/auto-dim/with-turn-off-and-restore* (lambda () ,@body)))
+
+;;;; ___________________________________________________________________________
+
 (provide 'nomis-window-backgrounds)
