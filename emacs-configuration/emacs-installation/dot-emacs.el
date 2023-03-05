ecc86f517fa2067bebcd394a7b64aa4ee6f680bd
Simon Katz <sk@nomistech.com>
2023-03-05 00:01:34 +0000

    Add `nomis-nav-flash` with `nav-flash-delay` set to `0.75`

--- emacs-configuration/emacs-installation/dot-emacs.el
+++ emacs-configuration/emacs-installation/dot-emacs.el
@@ -296,6 +296,8 @@
 
 (require 'nomis-auto-save-hacks)
 
+(require 'nomis-nav-flash)
+
 ;; (eval-after-load "isearch" '(require 'isearch+))
 ;; Is this the cause of problems with Magit when trying to commit?...
 ;; -- *ERROR*: ‘recenter’ing a window that does not display current-buffer.
