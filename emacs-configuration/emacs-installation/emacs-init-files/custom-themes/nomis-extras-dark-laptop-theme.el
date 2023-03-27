369d093474a1533a68e709c820aa6c02986e9e91
Simon Katz <sk@nomistech.com>
2023-03-27 23:16:13 +0100

    Set `company` faces in `nomis-extras-dark-laptop`
    Before this you couldn't see the current selection.

--- emacs-configuration/emacs-installation/emacs-init-files/custom-themes/nomis-extras-dark-laptop-theme.el
+++ emacs-configuration/emacs-installation/emacs-init-files/custom-themes/nomis-extras-dark-laptop-theme.el
@@ -1,23 +1,37 @@
 (deftheme nomis-extras-dark-laptop
   "Created 2023-02-01.")
 
+(require 'color)
+
 (defconst nomis-extras-dark-laptop/default-background
   "Black")
 
 (defconst nomis-extras-dark-laptop/auto-dim-background
   "Grey32")
 
-(custom-theme-set-faces
- 'nomis-extras-dark-laptop
+(let ((bg nomis-extras-dark-laptop/auto-dim-background))
 
- `(auto-dim-other-buffers-face
-   ((t  ,(list :background nomis-extras-dark-laptop/auto-dim-background))))
+  (custom-theme-set-faces
+   'nomis-extras-dark-laptop
 
- `(auto-dim-other-buffers-hide-face
-   ((t  ,(list :foreground nomis-extras-dark-laptop/auto-dim-background
-               :background nomis-extras-dark-laptop/auto-dim-background))))
+   `(auto-dim-other-buffers-face
+     ((t  ,(list :background bg))))
 
- `(org-hide
-   ((t ,(list :foreground nomis-extras-dark-laptop/default-background)))))
+   `(auto-dim-other-buffers-hide-face
+     ((t  ,(list :foreground bg
+                 :background bg))))
+
+   `(org-hide
+     ((t ,(list :foreground nomis-extras-dark-laptop/default-background))))
+
+   ;; The following is based on info at
+   ;; https://www.emacswiki.org/emacs/CompanyMode
+   `(company-scrollbar-bg
+     ((t `(list :background (color-lighten-name bg 40)))))
+   `(company-scrollbar-fg
+     ((t ,(list :background (color-lighten-name bg 20)))))
+   `(company-tooltip-selection
+     ((t ,(list :background "Purple4"
+                :foreground "PaleTurquoise"))))))
 
 (provide-theme 'nomis-extras-dark-laptop)
