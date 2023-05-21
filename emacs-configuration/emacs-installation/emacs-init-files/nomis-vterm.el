294d1b263288aabd20c3c263c918653dccfecb35
Simon Katz <sk@nomistech.com>
2023-05-21 20:36:20 +0100

    Add `(setq vterm-copy-mode-remove-fake-newlines t)`

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-vterm.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-vterm.el
@@ -2,12 +2,20 @@
 
 ;;;; ___________________________________________________________________________
 
+;;;; Emacs libvterm README says
+;;;;   And add this to your init.el:
+;;;;     (add-to-list 'load-path "path/to/emacs-libvterm")
+;;;;     (require 'vterm)
+
 (add-to-list 'load-path "/Users/simonkatz/development-100/repositories/software-installations/emacs-libvterm")
+
+;; (require 'vterm) ; Not needed. (But see abovedocumentation.)
 
 (with-eval-after-load 'vterm
 
   (setq vterm-max-scrollback 100000)
   (setq vterm-clear-scrollback-when-clearing t)
+  (setq vterm-copy-mode-remove-fake-newlines t)
 
   (define-key vterm-mode-map (kbd "M-k") 'vterm-clear))
 
