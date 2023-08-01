8d8446829631deecfc8554a2499500beb6016eb9
Simon Katz <sk@nomistech.com>
2023-08-01 11:18:27 +0100

    Change `sidecar-locals-dir-name` so that `nomis-do-to-all-git-repos` sees it

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-sidecar-locals.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-sidecar-locals.el
@@ -7,6 +7,8 @@
 (setq sidecar-locals-paths-allow
       '("/Users/simonkatz/development-100/repositories/"))
 
+(setq sidecar-locals-dir-name "temp-name-sidecar-locals") ; TODO: Hack: For `nomis-do-to-all-git-repos`, can't have names beginning with "."
+
 ;;;; ___________________________________________________________________________
 
 (provide 'nomis-sidecar-locals)
