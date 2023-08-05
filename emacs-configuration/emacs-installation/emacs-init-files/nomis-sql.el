5b40570d6ca027e9a7389769ecf658a87ffabaa2
Simon Katz <sk@nomistech.com>
2023-08-05 21:21:57 +0100

    WIP: sql-send-string text in inferior buffer

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-sql.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-sql.el
@@ -19,7 +19,51 @@
                                 (point)))))
 
   ;; I'm used to this key binding in TablePlus:
-  (define-key sql-mode-map (kbd "M-RET") 'sql-send-paragraph))
+  (define-key sql-mode-map (kbd "M-RET") 'sql-send-paragraph)
+
+
+
+  ;; HACKY WIP:
+  
+  (defface nomis/sql-separator-face
+    '((((background dark)) (:foreground "Green2" :italic t))
+      (t (:foreground "Green4")))
+    "Face for reloading messages.")
+
+  (defun sql-send-string (str)
+    "Send the string STR to the SQL process."
+    (interactive "sSQL Text: ")
+
+    (let ((comint-input-sender-no-newline nil)
+          (s (replace-regexp-in-string "[[:space:]\n\r]+\\'" "" str)))
+      (if (sql-buffer-live-p sql-buffer)
+          (progn
+            ;; Ignore the hoping around...
+            (save-excursion
+              ;; Set product context
+              (with-current-buffer sql-buffer
+                (when sql-debug-send
+                  (message ">>SQL> %S" s))
+                (insert "\n________________________________________\n")
+                (insert "Query:")
+                (insert s)
+                (insert "\n\nResult:\n")
+                (comint-set-process-mark)
+
+                ;; Send the string (trim the trailing whitespace)
+                (sql-input-sender (get-buffer-process (current-buffer)) s)
+
+                ;; Send a command terminator if we must
+                (sql-send-magic-terminator sql-buffer s sql-send-terminator)
+
+                (when sql-pop-to-buffer-after-send-region
+                  (message "Sent string to buffer %s" sql-buffer))))
+
+            ;; Display the sql buffer
+            (sql-display-buffer sql-buffer))
+
+        ;; We don't have no stinkin' sql
+        (user-error "No SQL process started")))))
 
 ;;;; ___________________________________________________________________________
 
