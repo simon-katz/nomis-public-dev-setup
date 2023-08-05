4ef9062b643c686d4a8c5047697107bac09dc2c1
Simon Katz <sk@nomistech.com>
2023-08-05 21:16:10 +0100

    Customise `sql-send-paragraph`: `M-RET` and `eval-sexp-fu-flash`

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-sql.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-sql.el
@@ -7,7 +7,19 @@
   (defun sql-send-buffer ()
     "Send the buffer contents to the SQL process."
     (interactive)
-    (message-box "sql-send-buffer is disabled — too easy to do it accidentally.")))
+    (message-box "sql-send-buffer is disabled — too easy to do it accidentally."))
+
+  (require 'eval-sexp-fu)
+  (define-eval-sexp-fu-flash-command sql-send-paragraph
+    (eval-sexp-fu-flash (cons (save-excursion
+                                (backward-paragraph)
+                                (point))
+                              (save-excursion
+                                (forward-paragraph)
+                                (point)))))
+
+  ;; I'm used to this key binding in TablePlus:
+  (define-key sql-mode-map (kbd "M-RET") 'sql-send-paragraph))
 
 ;;;; ___________________________________________________________________________
 
