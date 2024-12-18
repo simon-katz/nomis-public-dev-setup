5ac9bd4d9ad28c1441a5a71281988da3afcdb5b4
Simon Katz <sk@nomistech.com>
2024-12-18 13:44:48 +0000

    Turn off `highlight-indentation-mode` for `yaml-mode`

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-yaml.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-yaml.el
@@ -9,7 +9,7 @@
   (nomis/set-80-column-stuff-0))
 
 (add-hook 'yaml-mode-hook 'nomis/setup-yaml-mode)
-(add-hook 'yaml-mode-hook 'highlight-indentation-mode)
+;; (add-hook 'yaml-mode-hook 'highlight-indentation-mode)
 (add-hook 'yaml-mode-hook 'highlight-indentation-current-column-mode)
 (add-hook 'yaml-mode-hook 'yafolding-mode)
 (add-hook 'yaml-mode-hook 'nomis/idle-highlight-mode)
