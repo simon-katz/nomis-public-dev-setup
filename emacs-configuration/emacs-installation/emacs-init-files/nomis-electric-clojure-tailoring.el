45aef9b0351f65a5c8f79c133f97783708d291ea
Simon Katz <sk@nomistech.com>
2025-03-26 16:10:41 +0000

    Make Electric Clojure background colors more subtle

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-electric-clojure-tailoring.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-electric-clojure-tailoring.el
@@ -1,6 +1,17 @@
 ;;;; nomis-electric-clojure-tailoring.el --- nomis-electric-clojure-mode tailoring ---  -*- lexical-binding: t -*-
 
 ;;;; ___________________________________________________________________________
+;;;; Make background colors more subtle.
+
+(face-spec-set 'nomis/ec-client-face/using-background
+  `((((background dark)) ,(list :background "#005500"))
+    (t ,(list :background "DarkSeaGreen1"))))
+
+(face-spec-set 'nomis/ec-server-face/using-background
+  `((((background dark)) ,(list :background "#700000"))
+    (t ,(list :background "#ffc5c5"))))
+
+;;;; ___________________________________________________________________________
 
 (define-key clojure-mode-map (kbd "M-E") 'nomis-electric-clojure-mode)
 (define-key clojure-mode-map (kbd "C-M-e") 'nomis/ec-cycle-options)
