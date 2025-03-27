4bdb2288f35c0a44e23f1363abd6cda4a840355a
Simon Katz <sk@nomistech.com>
2025-03-27 15:30:59 +0000

    Add hacky stuff to make tooltip messages appear in the *Messages* buffer

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-tooltips.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-tooltips.el
@@ -6,4 +6,19 @@
 
 ;;;; ___________________________________________________________________________
 
+;; Some hacky stuff for making tooltip messages appear in the *Messages* buffer:
+
+;; ;; Reset (assuming `tooltip-mode` is on):
+;; (setq show-help-function 'tooltip-show-help); the default (when using tooltips)
+
+;; ;; Send to *Messages* buffer:
+
+;; (defun -nomis/show-help-in-messages-buffer-too (msg)
+;;   (let* ((inhibit-message t)) (message "%s" msg))
+;;   (tooltip-show-help msg))
+
+;; (setq show-help-function '-nomis/show-help-in-messages-buffer-too)
+
+;;;; ___________________________________________________________________________
+
 (provide 'nomis-tooltips)
