deb0d07ca67301b106b66337c387b1ad8d780a1f
Simon Katz <sk@nomistech.com>
2025-05-04 18:47:25 +0100

    Add `nomis/run-clojure-and-display-message`

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-run-clojure.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-run-clojure.el
@@ -35,6 +35,23 @@
                              (cider-emit-interactive-eval-err-output err))
                            '())))
 
+(defun nomis/run-clojure-and-display-message (clojure-form-as-string
+                                              &optional after-fun)
+  (cider-interactive-eval clojure-form-as-string
+                          (nrepl-make-response-handler
+                           (current-buffer)
+                           (lambda (buffer value)
+                             (prog1
+                                 (let ((value (transform-string-value value)))
+                                   (message "%s" (substring value 1 -1)))
+                               (when after-fun
+                                 (funcall after-fun))))
+                           (lambda (_buffer out)
+                             (cider-emit-interactive-eval-output out))
+                           (lambda (_buffer err)
+                             (cider-emit-interactive-eval-err-output err))
+                           '())))
+
 (defun nomis/run-clojure-no-insert (clojure-form-as-string)
   (cider-interactive-eval clojure-form-as-string
                           (nrepl-make-response-handler
