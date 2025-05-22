1a43cba4ffca02ec15a53ad60fab0eaa739a79df
Simon Katz <sk@nomistech.com>
2025-05-22 14:00:06 +0100

    Make priorities in `nomis-overlay-priorities` less intrusive

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-overlay-priorities.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-overlay-priorities.el
@@ -3,8 +3,8 @@
 ;;;; ___________________________________________________________________________
 
 (setq nomis/ec-base-priority-for-overlays 0) ; the default, but good to see here
-(setq hlt-overlays-priority         100000)  ; used in `nomis-idle-highlight-mode`
-(setq -nomis/lsp-highlight-priority 100001)  ; highest priority -- in-scope stuff
+(setq hlt-overlays-priority         '(nil . 100000))  ; used in `nomis-idle-highlight-mode`
+(setq -nomis/lsp-highlight-priority '(nil . 100001))  ; highest priority -- in-scope stuff
 
 ;;;; ___________________________________________________________________________
 
