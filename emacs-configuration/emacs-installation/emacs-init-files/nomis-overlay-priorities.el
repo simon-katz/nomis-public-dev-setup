4963a657ef684ec173cf5b8584a25d0c3313c9b4
Simon Katz <sk@nomistech.com>
2025-05-22 16:24:42 +0100

    Improve highlighting of the region

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-overlay-priorities.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-overlay-priorities.el
@@ -2,9 +2,13 @@
 
 ;;;; ___________________________________________________________________________
 
+;;;; Note that the region's overlay has priority `(nil . 100)` -- see
+;;;; `redisplay--highlight-overlay-function`. So we want to have values less
+;;;; than 100. (I found this out at https://emacs.stackexchange.com/q/17324)
+
 (setq nomis/ec-base-priority-for-overlays 0) ; the default, but good to see here
-(setq hlt-overlays-priority         '(nil . 100000))  ; used in `nomis-idle-highlight-mode`
-(setq -nomis/lsp-highlight-priority '(nil . 100001))  ; highest priority -- in-scope stuff
+(setq hlt-overlays-priority         '(nil . 98))  ; used in `nomis-idle-highlight-mode`
+(setq -nomis/lsp-highlight-priority '(nil . 99))  ; highest priority -- in-scope stuff
 
 ;;;; ___________________________________________________________________________
 
