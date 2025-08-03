8d51b9409eeec29d88da28f5e6a703e4e910be1e
Simon Katz <sk@nomistech.com>
2025-08-03 22:17:15 +0100

    Call `(nomis/wwo/set-binary-encoding-0)` for `eca-chat-mode`

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-eca.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-eca.el
@@ -4,6 +4,16 @@
 
 (require 'eca)
 
+;;;; ___________________________________________________________________________
+
+(defun nomis/setup-eca-chat-mode ()
+  (nomis/wwo/mode)
+  (nomis/wwo/set-binary-encoding-0))
+
+(add-hook 'eca-chat-mode-hook 'nomis/setup-eca-chat-mode)
+
+;;;; ___________________________________________________________________________
+
 (keymap-set eca-chat-mode-map "H-k" #'eca-chat-clear)
 (keymap-set eca-chat-mode-map "M-p" #'eca-chat--key-pressed-previous-prompt-history)
 (keymap-set eca-chat-mode-map "M-n" #'eca-chat--key-pressed-next-prompt-history)
