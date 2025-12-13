ed44fe072a53adc9c0a4073491dc158e2fdb7566
Simon Katz <sk@nomistech.com>
2025-12-13 11:40:54 +0000

    Use `setopt` in `beacon-mode` tailoring

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-beacon.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-beacon.el
@@ -7,17 +7,17 @@
 ;;;; ___________________________________________________________________________
 
 (beacon-mode 1)
-(setq beacon-blink-when-point-moves-horizontally 10)
-(setq beacon-blink-when-point-moves-vertically 10)
-(setq beacon-blink-duration 0.5)
-(setq beacon-blink-delay 0.1)
+(setopt beacon-blink-when-point-moves-horizontally 10)
+(setopt beacon-blink-when-point-moves-vertically 10)
+(setopt beacon-blink-duration 0.5)
+(setopt beacon-blink-delay 0.1)
 
 (define-key global-map (kbd "H-b") 'beacon-blink)
 
 (defun nomis/beacon/set-beacon-color ()
-  (setq beacon-color (if (nomis/dark-background-mode?)
-                         "Yellow"
-                       "Tomato")))
+  (setopt beacon-color (if (nomis/dark-background-mode?)
+                           "Yellow"
+                         "Tomato")))
 
 (add-hook 'emacs-startup-hook
           'nomis/beacon/set-beacon-color)
