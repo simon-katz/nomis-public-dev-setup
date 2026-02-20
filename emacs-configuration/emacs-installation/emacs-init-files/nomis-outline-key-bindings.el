eb5538cd62da9208bc5e093aa15529b3aa17685b
Simon Katz <sk@nomistech.com>
2026-02-20 11:03:15 +0000

    Add more headings in `nomis-outline-key-bindings`

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-outline-key-bindings.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-outline-key-bindings.el
@@ -23,11 +23,17 @@
 ;; | (kbd "C-H-\\")    | nomis/org-visibility-span/more                          |                                        |                             |
 ;; | (kbd "C-H-M-'")   | nomis/org-visibility-span/set-min                       |                                        |                             |
 ;; | (kbd "C-H-M-\\")  | nomis/org-visibility-span/set-max                       |                                        | nomis/outline-show-max-lineage |
+
+;; |-------------------+---------------------------------------------------------+----------------------------------------+-----------------------------|
+;; |                   | ---- Tree only / fat parents and max lineage ----       |                                        |                             |
 ;; | (kbd "H-M-m")     | norg/show-tree-only                                     |                                        | nomis/outline-show-fat-parents |
 
 (define-key outline-minor-mode-map (kbd "C-H-M-\\")     'nomis/outline-show-max-lineage)
 (define-key outline-minor-mode-map (kbd "H-M-m")        'nomis/outline-show-fat-parents)
 
+
+;; |-------------------+---------------------------------------------------------+----------------------------------------+-----------------------------|
+;; |                   | ---- Span levels to show ----                           |                                        |                             |
 ;; | (kbd "H-q H-q s") | norg/set-step-n-levels-to-show                          |                                        |                             |
 
 ;; |-------------------+---------------------------------------------------------+----------------------------------------+-----------------------------|
