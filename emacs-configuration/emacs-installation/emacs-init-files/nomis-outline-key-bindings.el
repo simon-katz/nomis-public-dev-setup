f2602f91828a269c162dc3f40226fd653b636c28
Simon Katz <sk@nomistech.com>
2026-02-20 23:20:26 +0000

    Delete hide-show column in nomis-outline-key-bindings.el

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-outline-key-bindings.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-outline-key-bindings.el
@@ -11,86 +11,86 @@
 
 ;;;; Key bindings for outline-minor-mode-map, cross-referncing `org-mode` mode and `hs-minor-mode`
 
-;; |-------------------+---------------------------------------------------------+----------------------------------------+-----------------------------|
-;; | Key               | org mode                                                | hide-show                              | outline                     |
+;; |-------------------+---------------------------------------------------------+-----------------------------|
+;; | Key               | org mode                                                | outline                     |
 
-;; |-------------------+---------------------------------------------------------+----------------------------------------+-----------------------------|
-;; |                   | ---- Expand/collapse ----                               |                                        |                             |
-;; | (kbd "H-'")       | norg/show-children-from-point/incremental/less          | nomis/hs/adjust/less                   |                             |
-;; | (kbd "H-\\")      | norg/show-children-from-point/incremental/more          | nomis/hs/adjust/more                   |                             |
-;; | (kbd "H-M-'")     | norg/show-children-from-point/set-min                   | nomis/hs/adjust/set-min                |                             |
-;; | (kbd "H-M-\\")    | norg/show-children-from-point/fully-expand              | nomis/hs/adjust/show-all               |                             |
-;; |                   |                                                         |                                        |                             |
-;; | (kbd "H-\"")      | norg/show-children-from-root/incremental/less           |                                        |                             |
-;; | (kbd "H-")        | norg/show-children-from-root/incremental/more           |                                        |                             |
-;; | (kbd "H-M-\"")    | norg/show-children-from-root/set-min                    |                                        |                             |
-;; | (kbd "H-M-")      | norg/show-children-from-root/fully-expand               |                                        |                             |
-;; |                   |                                                         |                                        |                             |
-;; | (kbd "H-[")       | norg/show-children-from-parent/incremental/less         |                                        |                             |
-;; | (kbd "H-]")       | norg/show-children-from-parent/incremental/more         |                                        |                             |
-;; | (kbd "H-M-[")     | norg/show-children-from-parent/set-min                  | nomis/hs/adjust/set-min-for-top-level  |                             |
-;; | (kbd "H-M-]")     | norg/show-children-from-parent/fully-expand             | nomis/hs/adjust/show-all-for-top-level |                             |
-;; |                   |                                                         |                                        |                             |
-;; | (kbd "H--")       | norg/show-children-from-all-roots/incremental/less      |                                        |                             |
-;; | (kbd "H-=")       | norg/show-children-from-all-roots/incremental/more      |                                        |                             |
-;; | (kbd "H-M--")     | norg/show-children-from-all-roots/set-min               | nomis/hs/hide-all                      |                             |
-;; | (kbd "H-M-=")     | norg/show-children-from-all-roots/fully-expand          | nomis/hs/show-all                      | Note: "H-o a" for outline-show-all |
-;; |                   |                                                         |                                        |                             |
-;; | (kbd "H-q H-q ]") | norg/show-children-from-root/to-current-level           |                                        |                             |
-;; | (kbd "H-q H-q =") | norg/show-children-from-all-roots/to-current-level      |                                        |                             |
-;; |                   |                                                         |                                        |                             |
-;; |                   | - The following keys are copied from org.el.            |                                        |                             |
-;; |                   | -- TAB key with modifiers                               |                                        |                             |
-;; | "\C-i"            | norg/cycle                                              |                                        |                             |
-;; | [(tab)]           | norg/cycle                                              |                                        | nomis/outline-tab                            |
-;; |                   | -- The following line is necessary under Suse GNU/Linux |                                        |                             |
-;; | [S-iso-lefttab]   | norg/shifttab                                           |                                        |                             |
-;; | [(shift tab)]     | norg/shifttab                                           |                                        | nomis/outline-dec-children                            |
-;; | [backtab]         | norg/shifttab                                           |                                        |                             |
+;; |-------------------+---------------------------------------------------------+-----------------------------|
+;; |                   | ---- Expand/collapse ----                               |                             |
+;; | (kbd "H-'")       | norg/show-children-from-point/incremental/less          |                             |
+;; | (kbd "H-\\")      | norg/show-children-from-point/incremental/more          |                             |
+;; | (kbd "H-M-'")     | norg/show-children-from-point/set-min                   |                             |
+;; | (kbd "H-M-\\")    | norg/show-children-from-point/fully-expand              |                             |
+;; |                   |                                                         |                             |
+;; | (kbd "H-\"")      | norg/show-children-from-root/incremental/less           |                             |
+;; | (kbd "H-")        | norg/show-children-from-root/incremental/more           |                             |
+;; | (kbd "H-M-\"")    | norg/show-children-from-root/set-min                    |                             |
+;; | (kbd "H-M-")      | norg/show-children-from-root/fully-expand               |                             |
+;; |                   |                                                         |                             |
+;; | (kbd "H-[")       | norg/show-children-from-parent/incremental/less         |                             |
+;; | (kbd "H-]")       | norg/show-children-from-parent/incremental/more         |                             |
+;; | (kbd "H-M-[")     | norg/show-children-from-parent/set-min                  |                             |
+;; | (kbd "H-M-]")     | norg/show-children-from-parent/fully-expand             |                             |
+;; |                   |                                                         |                             |
+;; | (kbd "H--")       | norg/show-children-from-all-roots/incremental/less      |                             |
+;; | (kbd "H-=")       | norg/show-children-from-all-roots/incremental/more      |                             |
+;; | (kbd "H-M--")     | norg/show-children-from-all-roots/set-min               |                             |
+;; | (kbd "H-M-=")     | norg/show-children-from-all-roots/fully-expand          | Note: "H-o a" for outline-show-all |
+;; |                   |                                                         |                             |
+;; | (kbd "H-q H-q ]") | norg/show-children-from-root/to-current-level           |                             |
+;; | (kbd "H-q H-q =") | norg/show-children-from-all-roots/to-current-level      |                             |
+;; |                   |                                                         |                             |
+;; |                   | - The following keys are copied from org.el.            |                             |
+;; |                   | -- TAB key with modifiers                               |                             |
+;; | "\C-i"            | norg/cycle                                              |                             |
+;; | [(tab)]           | norg/cycle                                              | nomis/outline-tab                            |
+;; |                   | -- The following line is necessary under Suse GNU/Linux |                             |
+;; | [S-iso-lefttab]   | norg/shifttab                                           |                             |
+;; | [(shift tab)]     | norg/shifttab                                           | nomis/outline-dec-children                            |
+;; | [backtab]         | norg/shifttab                                           |                             |
 
 (define-key outline-minor-mode-map [tab]               'nomis/outline-tab)
 (define-key outline-minor-mode-map [S-tab]             'nomis/outline-dec-children)
 (define-key outline-minor-mode-map (kbd "H-o <tab>")   'bicycle-cycle-global)
 
-;; |-------------------+---------------------------------------------------------+----------------------------------------+---------------------------------------------------|
-;; |                   | ---- Movement ----                                      |                                        |                                                   |
-;; | (kbd "H-,")       | norg/backward-heading-same-level                        |                                        | nomis/outline-previous-sibling                    |
-;; | (kbd "H-.")       | norg/forward-heading-same-level                         |                                        | nomis/outline-next-sibling                        |
-;; | (kbd "H-<")       | norg/backward-heading-same-level/allow-cross-parent     |                                        | nomis/outline-previous-sibling/allow-cross-parent |
-;; | (kbd "H->")       | norg/forward-heading-same-level/allow-cross-parent      |                                        | nomis/outline-next-sibling/allow-cross-parent     |
+;; |-------------------+---------------------------------------------------------+---------------------------------------------------|
+;; |                   | ---- Movement ----                                      |                                                   |
+;; | (kbd "H-,")       | norg/backward-heading-same-level                        | nomis/outline-previous-sibling                    |
+;; | (kbd "H-.")       | norg/forward-heading-same-level                         | nomis/outline-next-sibling                        |
+;; | (kbd "H-<")       | norg/backward-heading-same-level/allow-cross-parent     | nomis/outline-previous-sibling/allow-cross-parent |
+;; | (kbd "H->")       | norg/forward-heading-same-level/allow-cross-parent      | nomis/outline-next-sibling/allow-cross-parent     |
 
 (define-key outline-minor-mode-map (kbd "H-,")         'nomis/outline-previous-sibling)
 (define-key outline-minor-mode-map (kbd "H-.")         'nomis/outline-next-sibling)
 (define-key outline-minor-mode-map (kbd "H-<")         'nomis/outline-previous-sibling/allow-cross-parent)
 (define-key outline-minor-mode-map (kbd "H->")         'nomis/outline-next-sibling/allow-cross-parent)
 
-;; |-------------------+---------------------------------------------------------+----------------------------------------+-----------------------------|
-;; |                   | ---- Movement + expand/collapse ----                    |                                        |                             |
-;; | (kbd "H-M-,")     | norg/step-backward                                      | *WAS* nomis/hs/step-backward           | nomis/outline-step-backward |
-;; | (kbd "H-M-.")     | norg/step-forward                                       | *WAS* nomis/hs/step-forward            | nomis/outline-step-forward  |
-;; | (kbd "H-M-<")     | norg/step-backward/allow-cross-parent                   |                                        | nomis/outline-step-backward/allow-cross-parent |
-;; | (kbd "H-M->")     | norg/step-forward/allow-cross-parent                    |                                        | nomis/outline-step-forward/allow-cross-parent |
+;; |-------------------+---------------------------------------------------------+-----------------------------|
+;; |                   | ---- Movement + expand/collapse ----                    |                             |
+;; | (kbd "H-M-,")     | norg/step-backward                                      | nomis/outline-step-backward |
+;; | (kbd "H-M-.")     | norg/step-forward                                       | nomis/outline-step-forward  |
+;; | (kbd "H-M-<")     | norg/step-backward/allow-cross-parent                   | nomis/outline-step-backward/allow-cross-parent |
+;; | (kbd "H-M->")     | norg/step-forward/allow-cross-parent                    | nomis/outline-step-forward/allow-cross-parent |
 
 (define-key outline-minor-mode-map (kbd "H-M-,")       'nomis/outline-step-backward)
 (define-key outline-minor-mode-map (kbd "H-M-.")       'nomis/outline-step-forward)
 (define-key outline-minor-mode-map (kbd "H-M-<")       'nomis/outline-step-backward/allow-cross-parent)
 (define-key outline-minor-mode-map (kbd "H-M->")       'nomis/outline-step-forward/allow-cross-parent)
 
-;; |                   |                                                         |                                        |                             |
-;; | (kbd "C-H-,")     | norg/backward-heading/any-level                         |                                        | nomis/outline-previous-heading |
-;; | (kbd "C-H-.")     | norg/forward-heading/any-level                          |                                        | nomis/outline-next-heading  |
+;; |                   |                                                         |                             |
+;; | (kbd "C-H-,")     | norg/backward-heading/any-level                         | nomis/outline-previous-heading |
+;; | (kbd "C-H-.")     | norg/forward-heading/any-level                          | nomis/outline-next-heading  |
 
 (define-key outline-minor-mode-map (kbd "C-H-,")       'nomis/outline-previous-heading)
 (define-key outline-minor-mode-map (kbd "C-H-.")       'nomis/outline-next-heading)
 
-;; | (kbd "C-H-M-,")   | norg/backward-heading/any-level/set-tree+body           |                                        |                             |
-;; | (kbd "C-H-M-.")   | norg/forward-heading/any-level/set-tree+body            |                                        |                             |
-;; |                   |                                                         |                                        |                             |
-;; | (kbd "C-H-<")     | (*)                                                     |                                        |                             |
-;; | (kbd "C-H->")     | (*)                                                     |                                        |                             |
-;; | (kbd "C-H-M-<")   | (*)                                                     |                                        |                             |
-;; | (kbd "C-H-M->")   | (*)                                                     |                                        |                             |
-;; |-------------------+---------------------------------------------------------+----------------------------------------+-----------------------------|
+;; | (kbd "C-H-M-,")   | norg/backward-heading/any-level/set-tree+body           |                             |
+;; | (kbd "C-H-M-.")   | norg/forward-heading/any-level/set-tree+body            |                             |
+;; |                   |                                                         |                             |
+;; | (kbd "C-H-<")     | (*)                                                     |                             |
+;; | (kbd "C-H->")     | (*)                                                     |                             |
+;; | (kbd "C-H-M-<")   | (*)                                                     |                             |
+;; | (kbd "C-H-M->")   | (*)                                                     |                             |
+;; |-------------------+---------------------------------------------------------+-----------------------------|
 
 ;; (*) No real meaning -- with the M we are already crossing parent levels
 
