eef74930a986aedfd001ebea704297760920636e
Simon Katz <sk@nomistech.com>
2026-02-20 17:10:04 +0000

    Add `-nomis/ignore-all-keys-keymap`

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-very-general-stuff-new-lexical.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-very-general-stuff-new-lexical.el
@@ -2,6 +2,13 @@
 
 ;;; nomis/temporarily-disable-keys
 
+(defconst -nomis/ignore-all-keys-keymap
+  (let* ((km (make-sparse-keymap)))
+    (define-key km [t] (lambda ()
+                         (interactive)
+                         (nomis/msg/pulse-buffer-error)))
+    km))
+
 (defun nomis/temporarily-disable-keys (&optional pulse-on-restore?)
   ;; For a list of keymaps see
   ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Active-Keymaps.html
@@ -10,7 +17,7 @@
   (let* ((old-gkm (current-global-map))
          (old-lkm (current-local-map))
          (old-mmma minor-mode-map-alist))
-    (use-global-map (make-sparse-keymap))
+    (use-global-map -nomis/ignore-all-keys-keymap)
     (use-local-map nil)
     (setq minor-mode-map-alist nil)
     (run-at-time 1
