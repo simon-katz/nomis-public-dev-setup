ff3130c29a9edf0b3333f186f5bd109195b2a9a8
Simon Katz <sk@nomistech.com>
2026-03-07 13:12:44 +0000

    Add "Use of levels may be a bit messed up" to-do

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-outline-common.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-outline-common.el
@@ -100,6 +100,11 @@
     (:org     (org-end-of-line))))
 
 (defun nomis/outline/c/level (&optional inc-if-in-body?)
+  ;; TODO: Use of levels may be a bit messed up. Several things:
+  ;;       - The old `norg/current-level` had a guard, only incrementing
+  ;;         after checking `norg/show-bodies?`.
+  ;;       - We have `norg/level-incl-body/must-be-at-boh`. What's that for?
+  ;;         How does its use of `norg/show-bodies?` interact with this?
   (let* ((v (save-excursion (nomis/outline/c/back-to-heading)
                             (funcall outline-level))))
     (if (and inc-if-in-body?
