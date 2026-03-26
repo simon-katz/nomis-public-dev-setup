1f8b92725a0adbaf82ecd7c1b9546629f282682a
Simon Katz <sk@nomistech.com>
2026-03-26 18:32:51 +0000

    Change `nomis/themes/pairs` and show current selection when setting

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-themes.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-themes.el
@@ -2,7 +2,7 @@
 
 ;;;; ___________________________________________________________________________
 
-(require 'nomis-window-backgrounds)
+(require 'nomis-window-backgrounds) ; noflycheck
 
 ;;;; ___________________________________________________________________________
 
@@ -85,7 +85,7 @@
         (nomis/msg/grab-user-attention/low))
       (message "%sThemes are now: %s"
                (if wrapped? "(Wrapped) " "")
-               (nomis/themes/current-themes-as-string)))))
+               (-nomis/themes/current-themes-text)))))
 
 (defun nomis/themes/history/prev ()
   (interactive)
@@ -126,59 +126,58 @@
 (defconst nomis/themes/deeper-blue+altbg2          '(nomis-alternative-background-002                             nomis-common-dark  deeper-blue))
 (defconst nomis/themes/zenburn+altbg2              '(nomis-alternative-background-002                             nomis-common-dark  zenburn))
 
-(defconst nomis/themes/pairs
-  `(("Standard-Light + Nomis         " ,nomis/themes/standard-light+nomis)
-    ("Dark Laptop    + Nomis         " ,nomis/themes/dark-laptop+nomis)
-    ("Deeper-Blue    + Nomis         " ,nomis/themes/deeper-blue+nomis)
-    ("Zenburn        + Nomis         " ,nomis/themes/zenburn+nomis)
-    ("______                         " ())
-    ("Standard-Light + Nomis + AltBg1" ,nomis/themes/standard-light+nomis+altbg1)
-    ("Deeper-Blue    + Nomis + AltBg1" ,nomis/themes/deeper-blue+nomis+altbg1)
-    ("Zenburn        + Nomis + AltBg1" ,nomis/themes/zenburn+nomis+altbg1)
-    ("______                         " ())
-    ("Standard-Light + Nomis + AltBg2" ,nomis/themes/standard-light+nomis+altbg2)
-    ("Deeper-Blue    + Nomis + AltBg2" ,nomis/themes/deeper-blue+nomis+altbg2)
-    ("Zenburn        + Nomis + AltBg2" ,nomis/themes/zenburn+nomis+altbg2)
-    ("______                         " ())
-    ("Standard-Light                 " ,nomis/themes/standard-light)
-    ("Dark Laptop                    " ,nomis/themes/dark-laptop)
-    ("Deeper-Blue                    " ,nomis/themes/deeper-blue)
-    ("Zenburn                        " ,nomis/themes/zenburn)
-    ("______                         " ())
-    ("Standard-Light         + AltBg1" ,nomis/themes/standard-light+altbg1)
-    ("Deeper-Blue            + AltBg1" ,nomis/themes/deeper-blue+altbg1)
-    ("Zenburn                + AltBg1" ,nomis/themes/zenburn+altbg1)
-    ("______                         " ())
-    ("Standard-Light         + AltBg2" ,nomis/themes/standard-light+altbg2)
-    ("Deeper-Blue            + AltBg2" ,nomis/themes/deeper-blue+altbg2)
-    ("Zenburn                + AltBg2" ,nomis/themes/zenburn+altbg2)
-    ("===============================" ())))
+(defconst -nomis/themes/pairs
+  `((,nomis/themes/standard-light+nomis        . "Standard-Light + Nomis         ")
+    (,nomis/themes/dark-laptop+nomis           . "Dark Laptop    + Nomis         ")
+    (,nomis/themes/deeper-blue+nomis           . "Deeper-Blue    + Nomis         ")
+    (,nomis/themes/zenburn+nomis               . "Zenburn        + Nomis         ")
+    (()                                        . "______                         ")
+    (,nomis/themes/standard-light+nomis+altbg1 . "Standard-Light + Nomis + AltBg1")
+    (,nomis/themes/deeper-blue+nomis+altbg1    . "Deeper-Blue    + Nomis + AltBg1")
+    (,nomis/themes/zenburn+nomis+altbg1        . "Zenburn        + Nomis + AltBg1")
+    (()                                        . "______                         ")
+    (,nomis/themes/standard-light+nomis+altbg2 . "Standard-Light + Nomis + AltBg2")
+    (,nomis/themes/deeper-blue+nomis+altbg2    . "Deeper-Blue    + Nomis + AltBg2")
+    (,nomis/themes/zenburn+nomis+altbg2        . "Zenburn        + Nomis + AltBg2")
+    (()                                        . "______                         ")
+    (,nomis/themes/standard-light              . "Standard-Light                 ")
+    (,nomis/themes/dark-laptop                 . "Dark Laptop                    ")
+    (,nomis/themes/deeper-blue                 . "Deeper-Blue                    ")
+    (,nomis/themes/zenburn                     . "Zenburn                        ")
+    (()                                        . "______                         ")
+    (,nomis/themes/standard-light+altbg1       . "Standard-Light         + AltBg1")
+    (,nomis/themes/deeper-blue+altbg1          . "Deeper-Blue            + AltBg1")
+    (,nomis/themes/zenburn+altbg1              . "Zenburn                + AltBg1")
+    (()                                        . "______                         ")
+    (,nomis/themes/standard-light+altbg2       . "Standard-Light         + AltBg2")
+    (,nomis/themes/deeper-blue+altbg2          . "Deeper-Blue            + AltBg2")
+    (,nomis/themes/zenburn+altbg2              . "Zenburn                + AltBg2")
+    (()                                        . "===============================")))
 
-(defun -nomis/themes/themes->string (themes)
-  (let* ((current-pair (rassoc (list themes)
-                               nomis/themes/pairs)))
-    (message "current-pair = %s" current-pair)
-    (if current-pair
-        (->> current-pair
-           car
-           s-trim)
-      "Unknown theme set")))
+(defun -nomis/themes/themes-text (input-themes)
+  (cl-loop for (themes . text) in -nomis/themes/pairs
+           when (equal themes input-themes)
+           return text
+           finally (return "Unexpected value for custom-enabled-themes")))
 
-(defun nomis/themes/current-themes-as-string ()
-  (-nomis/themes/themes->string custom-enabled-themes))
+(defun -nomis/themes/current-themes-text ()
+  (-nomis/themes/themes-text custom-enabled-themes))
 
 (defun nomis/themes/set-custom-themes ()
   (interactive)
-  (let* ((prompt (format "Choose themes — currently %S: "
-                         (nomis/themes/current-themes-as-string)))
+  (let* ((curr-string (-nomis/themes/current-themes-text))
+         (prompt (format "Choose themes — currently %S: "
+                         curr-string))
          (response (ido-completing-read prompt
-                                        nomis/themes/pairs
+                                        (-map #'cdr -nomis/themes/pairs)
                                         nil
                                         t
                                         nil
-                                        'nomis/themes/set-custom-themes/prompt-history))
-         (chosen-themes (cadr (assoc response nomis/themes/pairs))))
-    (nomis/themes/disable-and-set-custom-themes chosen-themes)))
+                                        'nomis/themes/set-custom-themes/prompt-history
+                                        curr-string))
+         (v (car (rassoc response
+                         -nomis/themes/pairs))))
+    (nomis/themes/disable-and-set-custom-themes v)))
 
 ;;;; ___________________________________________________________________________
 
@@ -191,7 +190,7 @@
                 "                <empty>"
               (s-join "\n"
                       (->> themes*
-                           (-map #'-nomis/themes/themes->string)
+                           (-map #'-nomis/themes/themes-text)
                            (-map (lambda (s)
                                    (s-concat "                "
                                              s)))))))))
@@ -201,7 +200,7 @@
     (message "\n-nomis/themes/history/nexts:\n%s\n"
              (funcall themes*->string -nomis/themes/history/nexts)))
   (message "Current themes: %s\n(See *Messages* buffer for history list)"
-           (nomis/themes/current-themes-as-string)))
+           (-nomis/themes/current-themes-text)))
 
 ;;;; ___________________________________________________________________________
 
