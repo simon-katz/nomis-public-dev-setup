f2cecb37cd8b5d913567704254d741fd9a4081f0
Simon Katz <sk@nomistech.com>
2026-04-01 00:21:09 +0100

    Configure `which-key`

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-which-key.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-which-key.el
@@ -2,6 +2,21 @@
 
 (which-key-mode)
 
+(with-eval-after-load 'which-key
+  ;; (setopt which-key-allow-multiple-replacements t)
+  (setopt which-key-sort-order 'which-key-description-order)
+  (setopt which-key-replacement-alist
+          (seq-map
+           (lambda (rep)
+             `((nil . ,(elt rep 0))
+               . (nil . ,(elt rep 1))))
+           '(("nomis/tree/nav\\+lineage/" "")
+             ("nomis/tree/" "")
+             ("nomis/scrolling/toggle-maintain-line-no-in-window"
+              "toggle-maintain-line-no-in-window")
+             ("-nomis/tree-key-bindings/projectile-keybinding-error"
+              "ERROR (old Projectile)")))))
+
 ;;;; ___________________________________________________________________________
 
 (provide 'nomis-which-key)
