17b1220a17388c70a2b5bb9baf55133e6414728a
Simon Katz <sk@nomistech.com>
2026-05-18 20:24:18 +0100

    Change apps-and-libs to dev/apps-and-libs

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-ec-mode.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-ec-mode.el
@@ -1,1 +1,1 @@
-/Users/simonkatz/development-100/repositories/nomis/_apps-and-libs/nomis-electric-clojure-mode/nomis-ec-mode.el
+/Users/simonkatz/development-100/repositories/nomis/dev/apps-and-libs/nomis-electric-clojure-mode/nomis-ec-mode.el
