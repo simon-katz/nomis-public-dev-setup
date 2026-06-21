e01eae1b438db92ab553ade69a96cf0fcc7de971
Simon Katz <sk@nomistech.com>
2026-06-21 21:19:00 +0100

    Use `with-eval-after-load` instead of `eval-after-load`

--- emacs-configuration/emacs-installation/emacs-init-files/nomis-searching.el
+++ emacs-configuration/emacs-installation/emacs-init-files/nomis-searching.el
@@ -25,17 +25,17 @@
 ;;;; ___________________________________________________________________________
 ;;;; Better grepping.
 
-(eval-after-load "grep"
-  '(when (equal system-type 'darwin)
-     (grep-apply-setting
-      'grep-find-template
-      (cl-case 2
-        (1
-         ;; The default computed on Sheringham
-         "find <D> <X> -type f <F> -exec grep <C> -nH --null -e <R> \\{\\} +")
-        (2
-         ;; What I'm trying -- sort the files
-         "find <D> <X> -type f <F> -print0 | sort -z | xargs -0 grep <C> -nH --null -e <R>")))))
+(with-eval-after-load 'grep
+  (when (equal system-type 'darwin)
+    (grep-apply-setting
+     'grep-find-template
+     (cl-case 2
+       (1
+        ;; The default computed on Sheringham
+        "find <D> <X> -type f <F> -exec grep <C> -nH --null -e <R> \\{\\} +")
+       (2
+        ;; What I'm trying -- sort the files
+        "find <D> <X> -type f <F> -print0 | sort -z | xargs -0 grep <C> -nH --null -e <R>")))))
 
 ;;;; Based on code found at
 ;;;; https://github.com/magnars/.emacs.d/blob/master/setup-rgrep.el.
